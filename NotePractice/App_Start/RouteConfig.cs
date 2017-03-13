f103855f90d4fc12324ab3ea21a6ccd5bc0294a5
曾璐 <865514198@qq.com>
2017-03-13 22:50:34 +0800

    添加控制台项目

--- NotePractice/App_Start/RouteConfig.cs
+++ NotePractice/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NotePractice
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "FormSubmit", action = "FormMethod1", id = UrlParameter.Optional }
             );
         }
     }
