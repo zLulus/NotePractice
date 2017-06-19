a534a3ec31827c3401074495768f92f59578b223
曾璐 <865514198@qq.com>
2017-06-19 17:34:54 +0800

    CSS练习

---
+++ NotePractice/Controllers/AboutCSSController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotePractice.Controllers
+{
+    public class AboutCSSController : Controller
+    {
+        public ActionResult TomatoButton()
+        {
+            return View();
+        }
+    }
+}
