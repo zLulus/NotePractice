2128b75ea74ad62a139fe082b69bf317294d81c6
曾璐 <865514198@qq.com>
2017-06-28 18:03:39 +0800

    Vue

---
+++ NotePractice/Controllers/VueController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotePractice.Controllers
+{
+    public class VueController : Controller
+    {
+        // GET: Vue
+        public ActionResult HelloWorld()
+        {
+            return View();
+        }
+
+        public ActionResult Bind()
+        {
+            return View();
+        }
+        public ActionResult See()
+        {
+            return View();
+        }
+
+        public ActionResult For()
+        {
+            return View();
+        }
+    }
+}
