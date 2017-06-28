aca29314351e81d4d4733ac5cb443c8fbf14d3fb
曾璐 <865514198@qq.com>
2017-06-28 16:46:32 +0800

    js加密

--- NotePractice/Controllers/EncryptionController.cs
+++ NotePractice/Controllers/EncryptionController.cs
@@ -22,5 +22,10 @@ namespace NotePractice.Controllers
         {
             return View();
         }
+
+        public ActionResult EscapeView()
+        {
+            return View();
+        }
     }
 }
