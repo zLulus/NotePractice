1187b4caaf0118022e72a5a4e6a7443d57d3965e
曾璐 <865514198@qq.com>
2017-06-30 11:18:44 +0800

    异常捕获

--- WPF/WpfDemo/App.xaml.cs
+++ WPF/WpfDemo/App.xaml.cs
@@ -13,5 +13,18 @@ namespace WpfDemo
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            //wpf 程序异常捕获，而不崩溃退出
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("程序异常." + Environment.NewLine + e.Exception.Message);
+            //Shutdown(1);
+            //标记为异常已经处理
+            e.Handled = true;
+        }
     }
 }
