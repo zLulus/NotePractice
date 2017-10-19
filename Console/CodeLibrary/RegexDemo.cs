8edbf4ad47e76dff88824cd394642b84c6bd1569
曾璐 <865514198@qq.com>
2017-10-19 21:13:07 +0800

    正则表达式demo

---
+++ Console/CodeLibrary/RegexDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeLibrary
+{
+    public class RegexDemo
+    {
+        public static void Demo()
+        {
+            string ToBeRegex = @"m=39536; Domain=baidu.com; Expires=Sat, 21-Oct-2017 12:52:47 GMT; Path=/, u=130****7736; Domain=uspard.com; Expires=Sat, 21-Oct-2017 12:52:47 GMT; Path=/, rn=%E6%9B%BE%E5%9B%BD%E5%AF%8C; Domain=baidu.com; Expires=Sat, 21-Oct-2017 12:52:47 GMT; Path=/, lid=60637; Domain=baidu.com; Expires=Sat, 21-Oct-2017 12:52:47 GMT; Path=/, wx=""""; Domain=baidu.com; Expires=Sat, 21-Oct-2017 12:52:47 GMT; Path=/, bs=800301%2C800201; Domain=baidu.com; Expires=Sat, 21-Oct-2017 12:52:47 GMT; Path=/, t=1508417567138; Domain=baidu.com; Expires=Sat, 21-Oct-2017 12:52:47 GMT; Path=/, s=4fd514113bde1ce8c10058148cd16d6a; Domain=baidu.com; Expires=Sat, 21-Oct-2017 12:52:47 GMT; Path=/";
+            //.+?任意字符串
+            //()表示一个集合，?<name>为值命名
+            //IgnoreCase表示忽略大小写
+            Regex regex = new Regex("m=(?<m>.+?);.+?u=(?<u>.+?);",RegexOptions.IgnoreCase);
+            var matches = regex.Match(ToBeRegex);
+            if (matches.Success)
+            {
+                string m = matches.Groups["m"].Value;
+                string u = matches.Groups["u"].Value;
+            }
+        }
+    }
+}
--- Console/ConsoleDemo/Program.cs
+++ Console/ConsoleDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,19 +11,27 @@ namespace ConsoleDemo
     {
         static void Main(string[] args)
         {
+            //EnumTest();
+
+            //在正则表达式中给取得的值命名
+            RegexDemo.Demo();
+        }
+
+        private static void EnumTest()
+        {
             int i = 0;
             Console.WriteLine(i);
             Console.WriteLine(i.ToString());
             Console.WriteLine();
 
-            Student s = new Student() { Name = "YYY" };
+            Student s = new Student() {Name = "YYY"};
             Console.WriteLine(s);
             Console.WriteLine(s.ToString());
             Console.WriteLine();
 
             Console.WriteLine(Days.Friday);
             Console.WriteLine(Days.Friday.ToString());
-            Console.WriteLine((int)Days.Friday);
+            Console.WriteLine((int) Days.Friday);
 
             Console.Read();
         }
