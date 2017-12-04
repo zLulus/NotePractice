b4eb1e833a2b3ae65ac6a3c0872aac0813d41a1c
Lulus <865514198@qq.com>
2017-12-04 11:25:16 +0800

    IdentityServer.EasyDemo.IdentityServer

---
+++ IdentityServer/IdentityServer.EasyDemo/IdentityServer.EasyDemo.IdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityServer.EasyDemo.IdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ IdentityServer/IdentityServer.EasyDemo/IdentityServer.EasyDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityServer.EasyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
