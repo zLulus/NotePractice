8f9c01dff4564cc0a8ec74620c868ab013580ba2
Lulus <865514198@qq.com>
2017-12-04 12:11:36 +0800

    IdentityServer.EasyDemo.Client

---
+++ IdentityServer/IdentityServer.EasyDemo/IdentityServer.EasyDemo.Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityServer.EasyDemo.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                //从元数据中发现终结点,查找IdentityServer
+                var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+                if (disco.IsError)
+                {
+                    Console.WriteLine(disco.Error);
+                    return;
+                }
+
+                //向IdentityServer请求令牌
+                var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+                var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+                if (tokenResponse.IsError)
+                {
+                    Console.WriteLine(tokenResponse.Error);
+                    return;
+                }
+
+                Console.WriteLine(tokenResponse.Json);
+
+                //访问Api
+                var client = new HttpClient();
+                //把令牌添加进请求
+                client.SetBearerToken(tokenResponse.AccessToken);
+
+                var response = await client.GetAsync("http://localhost:5001/identity");
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(response.StatusCode);
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(JArray.Parse(content));
+                }
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
