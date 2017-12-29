981a99c440b0b94d4fa03ff9089dc204216fcc63
曾璐 <865514198@qq.com>
2017-12-29 16:23:10 +0800

    两个demo

---
+++ WPF/WpfDemo/Bind/BindDemoForINotifyPropertyChanged.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDemo.Bind.Models;
+
+namespace WpfDemo.Bind
+{
+    /// <summary>
+    /// BindDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class BindDemoForINotifyPropertyChanged : Window
+    {
+        public BindDemoForINotifyPropertyChanged()
+        {
+            InitializeComponent();
+            Student student = new Student()
+            {
+                Name = "Lulu"
+            };
+            DataContext = student;
+        }
+    }
+}
---
+++ WPF/WpfDemo/Bind/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.Bind.Models
+{
+    public class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+    }
+}
