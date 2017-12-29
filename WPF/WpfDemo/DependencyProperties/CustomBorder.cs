cfb9ef348df8a122a87be399b9d0f72dd96225a0
曾璐 <865514198@qq.com>
2017-12-29 16:53:18 +0800

    CustomBorder，自定义控件实现依赖属性

---
+++ WPF/WpfDemo/DependencyProperties/CustomBorder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfDemo.DependencyProperties
+{
+    public class CustomBorder : Border
+    {
+        public CustomBorder()
+        {
+            //初始化时默认给定一个背景色 
+            Background = Brushes.Blue;
+        }
+        public readonly static DependencyProperty TransparencyDependency =
+            DependencyProperty.Register( 
+            "Transparency",
+            typeof(Double),
+            typeof(CustomBorder),
+            new PropertyMetadata(new PropertyChangedCallback(transparencyPropertyChangedCallback))
+            );
+
+        static void transparencyPropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            CustomBorder border = (sender as CustomBorder);
+            if (border != null)
+            {
+                border.Opacity = 1 - Convert.ToDouble(e.NewValue) / 255;
+            }
+        }
+
+        public Double Transparency
+        {
+            get { return (Double)GetValue(TransparencyDependency); }
+            set { SetValue(TransparencyDependency, value); }
+        }
+    }
+}
