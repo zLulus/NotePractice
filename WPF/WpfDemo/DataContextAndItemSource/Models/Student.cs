5c32815664bfd1b8b70a97a9f874d583c4bec160
曾璐 <865514198@qq.com>
2017-12-30 18:56:29 +0800

    DataContext和ItemSource

---
+++ WPF/WpfDemo/DataContextAndItemSource/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.DataContextAndItemSource.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+    }
+}
