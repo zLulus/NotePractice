0fdc3ac9531c4243cb4d33bc6a7b5bc5eac2da61
曾璐 <865514198@qq.com>
2017-12-31 16:50:24 +0800

    VisualStateManagerDemo

---
+++ WPF/WpfDemo/VisualStateManager/VisualStateManagerDemo.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDemo.VisualStateManager
+{
+    /// <summary>
+    /// VisualStateManagerDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class VisualStateManagerDemo : Window
+    {
+        public VisualStateManagerDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("当前按钮可用");
+        }
+    }
+}
