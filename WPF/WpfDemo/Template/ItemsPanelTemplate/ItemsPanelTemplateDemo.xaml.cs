fccf982e5ea0906c7821507724c1cde2fbd4f831
曾璐 <865514198@qq.com>
2017-12-31 14:45:55 +0800

    ItemsPanelTemplateDemo

---
+++ WPF/WpfDemo/Template/ItemsPanelTemplate/ItemsPanelTemplateDemo.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDemo.Template.DataTemplate.Models;
+
+namespace WpfDemo.Template.ItemsPanelTemplate
+{
+    /// <summary>
+    /// ItemsPanelTemplateDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class ItemsPanelTemplateDemo : Window
+    {
+        public ItemsPanelTemplateDemo()
+        {
+            InitializeComponent();
+
+            ObservableCollection<Person> persons = new ObservableCollection<Person>();
+            persons.Add(new Person()
+            {
+                Name = "吴南",
+                Age = 20,
+                Hometown = "成都"
+            });
+            persons.Add(new Person()
+            {
+                Name = "刘艳",
+                Age = 21,
+                Hometown = "北京"
+            });
+            persons.Add(new Person()
+            {
+                Name = "刘芸",
+                Age = 19,
+                Hometown = "上海"
+            });
+            DataContext = persons;
+        }
+    }
+}
