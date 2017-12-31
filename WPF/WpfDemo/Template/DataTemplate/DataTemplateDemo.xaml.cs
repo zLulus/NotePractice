830a6c46ea2fbf4efdc4b5800e12753d693e71ed
曾璐 <865514198@qq.com>
2017-12-31 14:38:27 +0800

    DataTemplateDemo

---
+++ WPF/WpfDemo/Template/DataTemplate/DataTemplateDemo.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDemo.Template.DataTemplate.Models;
+
+namespace WpfDemo.Template.DataTemplate
+{
+    /// <summary>
+    /// DataTemplateDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataTemplateDemo : Window
+    {
+        public DataTemplateDemo()
+        {
+            InitializeComponent();
+
+            ObservableCollection<Person> persons = new ObservableCollection<Person>();
+            persons.Add(new Person()
+            {
+                Name = "吴南",
+                Age = 20,
+                Hometown = "成都"
+            });
+            persons.Add(new Person()
+            {
+                Name = "刘艳",
+                Age = 21,
+                Hometown = "北京"
+            });
+            persons.Add(new Person()
+            {
+                Name = "刘芸",
+                Age = 19,
+                Hometown = "上海"
+            });
+            DataContext = persons;
+        }
+    }
+}
---
+++ WPF/WpfDemo/Template/DataTemplate/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.Template.DataTemplate.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Hometown { get; set; }
+    }
+}
