b821db262cb55eaa17d9a99b49c214c95401cfd0
曾璐 <865514198@qq.com>
2017-12-31 16:42:26 +0800

    ChangeStudentInfo

---
+++ WPF/WpfDemo/PropertyChanged/ChangeStudentInfo.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDemo.PropertyChanged.Models;
+using WpfDemo.PropertyChanged.ViewModels;
+
+namespace WpfDemo.PropertyChanged
+{
+    /// <summary>
+    /// ChangeStudentInfo.xaml 的交互逻辑
+    /// </summary>
+    public partial class ChangeStudentInfo : Window
+    {
+        ChangeStudentInfoViewModel vm = new ChangeStudentInfoViewModel();
+        public ChangeStudentInfo()
+        {
+            InitializeComponent();
+            vm.InfoViewModel= new StudentByINotifyPropertyChanged()
+            {
+                Name = "小芳",
+                Sex = "女"
+            };
+            vm.TextBoxViewModel = new StudentByINotifyPropertyChanged();
+            DataContext = vm;
+        }
+
+        private void ChangeClick(object sender, RoutedEventArgs e)
+        {
+            vm.InfoViewModel.Name = vm.TextBoxViewModel.Name;
+            vm.InfoViewModel.Sex = vm.TextBoxViewModel.Sex;
+        }
+    }
+}
---
+++ WPF/WpfDemo/PropertyChanged/ViewModels/ChangeStudentInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDemo.PropertyChanged.Models;
+
+namespace WpfDemo.PropertyChanged.ViewModels
+{
+    public class ChangeStudentInfoViewModel
+    {
+        public StudentByINotifyPropertyChanged TextBoxViewModel { get; set; }
+        public StudentByINotifyPropertyChanged InfoViewModel { get; set; }
+    }
+}
