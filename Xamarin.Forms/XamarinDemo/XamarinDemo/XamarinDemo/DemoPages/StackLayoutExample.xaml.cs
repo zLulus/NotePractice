09ae0b83c8167d8fcaf0221a9621c0132a7cfc62
Lulus <865514198@qq.com>
2018-01-01 16:16:57 +0800

    StackLayoutExample2

--- Xamarin.Forms/XamarinDemo/XamarinDemo/XamarinDemo/DemoPages/StackLayoutExample.xaml.cs
+++ Xamarin.Forms/XamarinDemo/XamarinDemo/XamarinDemo/DemoPages/StackLayoutExample.xaml.cs
@@ -16,28 +16,37 @@ namespace XamarinDemo.DemoPages
     {
         public StackLayoutExample()
         {
+            //同时设置xaml和cs代码,会以cs代码为准
             var red = new Label
             {
                 Text = "Stop",
-                BackgroundColor = Color.Red,
+                BackgroundColor = Color.AliceBlue,
                 FontSize = 20
             };
             var yellow = new Label
             {
                 Text = "Slow down",
-                BackgroundColor = Color.Yellow,
+                BackgroundColor = Color.Pink,
                 FontSize = 20
             };
             var green = new Label
             {
                 Text = "Go",
-                BackgroundColor = Color.Green,
+                BackgroundColor = Color.YellowGreen,
                 FontSize = 20
             };
 
+            //内容
             Content = new StackLayout
             {
+                //间距
                 Spacing = 10,
+                //垂直方向上，从底部出发
+                VerticalOptions = LayoutOptions.End,
+                //堆放三个Label的方向是水平
+                Orientation = StackOrientation.Horizontal,
+                //水平方向上，从开始（左边）出发
+                HorizontalOptions = LayoutOptions.Start,
                 Children = { red, yellow, green }
             };
         }
