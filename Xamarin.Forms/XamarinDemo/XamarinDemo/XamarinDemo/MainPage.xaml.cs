ef31957f15b368df67c027c689ecd68b5d12ade6
Lulus <865514198@qq.com>
2018-01-02 12:00:38 +0800

    demo集合首页按钮跳转

--- Xamarin.Forms/XamarinDemo/XamarinDemo/XamarinDemo/MainPage.xaml.cs
+++ Xamarin.Forms/XamarinDemo/XamarinDemo/XamarinDemo/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinDemo.Bindings;
+using XamarinDemo.CustomizingCell;
 using XamarinDemo.DemoPages;
 
 namespace XamarinDemo
@@ -14,16 +16,20 @@ namespace XamarinDemo
 		{
 			InitializeComponent();
             Button StackLayoutDemo1Button = new Button();
-            StackLayoutDemo1Button.Clicked += ((sender,e)=>
-            {
-                Navigation.PushAsync(new StackLayoutExample());
-            });
+            StackLayoutDemo1Button.Clicked += StackLayoutDemo1Clicked;
             StackLayoutDemo1Button.Text = "StackLayout+Label";
+
             Button StackLayoutDemo1Button2 = new Button();
-            StackLayoutDemo1Button2.Clicked += ((sender,e)=> {
-                Navigation.PushModalAsync(new ListViewInStackLayout());
-            });
+            StackLayoutDemo1Button2.Clicked += StackLayoutDemo2Clicked;
             StackLayoutDemo1Button2.Text = "StackLayout+ListView";
+
+            Button CustomizingCellDemoButton1 = new Button();
+            CustomizingCellDemoButton1.Clicked += CustomizingCellDemoButton1Clicked;
+            CustomizingCellDemoButton1.Text = "自定义单元格Demo";
+
+            Button ShowTodoItemButton1 = new Button();
+            ShowTodoItemButton1.Clicked += ShowTodoItemButton1Clicked;
+            ShowTodoItemButton1.Text = "数据绑定";
 
             //内容
             Content = new StackLayout
@@ -36,6 +42,16 @@ namespace XamarinDemo
                     StackLayoutDemo1Button2
                 }
             };
+        }
+
+        private void ShowTodoItemButton1Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowTodoItem());
+        }
+
+        private void CustomizingCellDemoButton1Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CustomizingCellDemo());
         }
 
         public void StackLayoutDemo1Clicked(object sender, EventArgs e)
