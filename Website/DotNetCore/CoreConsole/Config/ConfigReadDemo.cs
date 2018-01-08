33d795451c55160c22052f51cbc202e3e60dc3c6
曾璐 <865514198@qq.com>
2018-01-08 18:03:48 +0800

    .net core读取配置文件

---
+++ Website/DotNetCore/CoreConsole/Config/ConfigReadDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreConsole
+{
+    public class ConfigReadDemo
+    {
+        public static void ReadConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            //引用
+            //json属性-复制
+            builder.AddJsonFile("ConfigTest.json");
+            var config = builder.Build();
+            Console.WriteLine(config["TotalCount"]);
+            Console.WriteLine(config["Students:0:Name"]);
+            Console.WriteLine(config["Students:0:Sex"]);
+            Console.WriteLine(config["Students:1:Name"]);
+            Console.WriteLine(config["Students:1:Sex"]);
+        }
+
+        public static void ReadConfigByBind()
+        {
+
+        }
+
+        public static void ReadConfigByOptions()
+        {
+
+        }
+
+        public static void ReadConfigHotUpdate()
+        {
+
+        }
+    }
+}
---
+++ Website/DotNetCore/CoreConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigReadDemo.ReadConfig();
+
+            Console.ReadLine();
+        }
+    }
+}
