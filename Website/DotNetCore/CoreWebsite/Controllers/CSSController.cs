2cf7d5ca4a7897aebeb476be5ce5f9d27d3ce922
曾璐 <865514198@qq.com>
2018-01-21 17:32:50 +0800

    Snowflake

---
+++ Website/DotNetCore/CoreWebsite/Controllers/CSSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.Controllers
+{
+    public class CSSController : Controller
+    {
+        public IActionResult Snowflake()
+        {
+            //原网址：http://ss.52hhx.com/login
+            return View();
+        }
+    }
+}
