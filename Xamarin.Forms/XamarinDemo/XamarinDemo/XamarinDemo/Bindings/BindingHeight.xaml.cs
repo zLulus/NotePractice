303709d22d4fd78cca7fb7445e223d02c4ffe28a
Lulus <865514198@qq.com>
2018-02-26 21:20:19 +0800

    GridLength绑定高度

---
+++ Xamarin.Forms/XamarinDemo/XamarinDemo/XamarinDemo/Bindings/BindingHeight.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinDemo.Bindings.Models;
+
+namespace XamarinDemo.Bindings
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BindingHeight : ContentPage
+	{
+		public BindingHeight ()
+		{
+			InitializeComponent ();
+            BindingContext = new HeightViewModel()
+            {
+                CustomHeight =200,
+                GridLength=new GridLength(200.00)
+            };
+        }
+	}
+}
