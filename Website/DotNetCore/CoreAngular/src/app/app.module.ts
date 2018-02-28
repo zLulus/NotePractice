2019bf1e9254eb0ca6ab224a9e3b67a8371d9a70
曾璐 <865514198@qq.com>
2018-02-28 18:10:55 +0800

    angular学习

--- Website/DotNetCore/CoreAngular/src/app/app.module.ts
+++ Website/DotNetCore/CoreAngular/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
