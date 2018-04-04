07bd76a7c70afb3d7fc34bca28d1ca89a319f5a5
曾璐 <865514198@qq.com>
2018-04-04 17:07:27 +0800

    C#调用js Jint库;添加一个富文本编辑器页面

---
+++ Console/CodeLibrary/ExcuteJs/ExcuteJsDemo.cs
@@ -0,0 +1,27 @@
+﻿using Jint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLibrary.ExcuteJs
+{
+    public class ExcuteJsDemo
+    {
+        public static void ExcuteJs()
+        {
+            //引用：Jint
+            string filePath = $"{Environment.CurrentDirectory}//ExcuteJs//TestJs.js";
+            string data1 = "1";
+            string data2 = "2";
+            string jsCode = System.IO.File.ReadAllText(filePath);
+            var square = new Engine()
+                                                    .SetValue("data1", data1) // define a new variable
+                                                    .SetValue("data2", data2) // define a new variable
+                                                    .Execute(jsCode) // execute a statement
+                                                    .GetCompletionValue() // get the latest statement completion value
+                                                    .ToObject(); // converts the value to .NET
+        }
+    }
+}
