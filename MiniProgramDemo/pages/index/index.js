f4e375c874fc05408d83f5db1a8737687a60c071
曾璐 <865514198@qq.com>
2018-04-25 09:17:09 +0000

    作用域demo

--- MiniProgramDemo/pages/index/index.js
+++ MiniProgramDemo/pages/index/index.js
@@ -5,13 +5,14 @@ Page({
   data: {
     apiData:{},
     sessionKey: "",
+    sessionKey2:""
   },
   onLoad: function () {
     
   },
   testApi: function() {
     var that = this;
-    //todo
+    //接口请参见：https://github.com/zLulus/NotePractice/blob/dev3/Website/DotNetFramework/NotePractice/Controllers/AccountForMiniProgramController.cs
     util.getRequest('/AccountForMiniProgram/GetData', that, "apiData");
     //此处打日志是拿不到数据的，因为是异步请求
   },
@@ -34,14 +35,28 @@ Page({
   },
   testScope2:function(){
     //参考资料：http://jsrocks.org/cn/2014/10/arrow-functions-and-their-scope
-    //=>
+    //使用=>  则作用域正确
+    wx.login({
+      success: (res)=> {
+        //this在里面
+        var that = this;
+        console.log(res);
+        if (res.code) {
+          //调用后端接口获得sessionkey
+          util.postRequest('/AccountForMiniProgram/WechatGetSessionKey', { id: res.code }, that, "sessionKey2");
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    });
   },
   testScope3:function(){
     wx.login({
       success: function (res) {
         //this在里面
+        //报错：that.setData is not a function   因为此时作用域已经改变
         var that = this;
-        console.log(res)
+        console.log(res);
         if (res.code) {
           //调用后端接口获得sessionkey
           util.postRequest('/AccountForMiniProgram/WechatGetSessionKey', { id: res.code }, that, "sessionKey");
