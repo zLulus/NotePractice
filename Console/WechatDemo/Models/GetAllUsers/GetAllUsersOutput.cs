000c3620910395b75ebcb1a00385345f65a063d6
曾璐 <865514198@qq.com>
2018-05-28 11:28:20 +0800

    微信查询所有用户

---
+++ Console/WechatDemo/Models/GetAllUsers/GetAllUsersOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WechatDemo.Models.GetAllUsers
+{
+    public class GetAllUsersOutput
+    {
+        public int total { get; set; }
+        public int count { get; set; }
+        public GetAllUsersData data { get; set; }
+        public string next_openid { get; set; }
+    }
+}
--- Console/WechatDemo/Program.cs
+++ Console/WechatDemo/Program.cs
@@ -1,16 +1,54 @@
-﻿using Senparc.Weixin.MP.AdvancedAPIs;
+﻿using Newtonsoft.Json;
+using Senparc.Weixin.MP.AdvancedAPIs;
 using Senparc.Weixin.MP.Containers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using WechatDemo.Models.GetAllUsers;
 
 namespace WechatDemo
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //查询目标用户的信息
+            //GetTargetUserInfo();
+
+            Task.Run(async () =>
+            {
+                //查询所有用户
+                await GetAllUsers();
+            });
+
+            Console.ReadLine();
+        }
+        private static async Task GetAllUsers()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var appId = "";
+                var appSecret = "";
+                //起始位置，为空则从头开始查询
+                var next_openid = "";
+                //注册一次即可
+                AccessTokenContainer.Register(appId, appSecret);
+                var token = AccessTokenContainer.GetAccessToken(appId);
+                string url = string.Format("https://api.weixin.qq.com/cgi-bin/user/get?access_token={0}", token);
+                if (!string.IsNullOrEmpty(next_openid))
+                {
+                    url += "&next_openid=" + next_openid;
+                }
+                var response = await client.GetAsync(url);
+                var resultStr = await response.Content.ReadAsStringAsync();
+                GetAllUsersOutput result = JsonConvert.DeserializeObject<GetAllUsersOutput>(resultStr);
+            }
+        }
+
+        private static void GetTargetUserInfo()
         {
             //微信查询用户信息
             string appId = "";
