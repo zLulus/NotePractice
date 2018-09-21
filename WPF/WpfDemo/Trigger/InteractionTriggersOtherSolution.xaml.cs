97382c908756a5bd7a3dc7ef13ace43a70b9a028
曾璐 <865514198@qq.com>
2018-09-21 17:25:06 +0800

    wpf Trigger

---
+++ WPF/WpfDemo/Trigger/InteractionTriggersOtherSolution.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDemo.Trigger.Models;
+
+namespace WpfDemo.Trigger
+{
+    /// <summary>
+    /// Interaction logic for InteractionTriggersNextSolution.xaml
+    /// </summary>
+    public partial class InteractionTriggersOtherSolution : Window
+    {
+        public InteractionTriggersOtherSolution()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowDialog(object sender, ShowDialogEventArgs e)
+        {
+            MessageBox.Show("ShowDialog");
+        }
+
+        public void AppointmentEditing(object sender, AppointmentEditingEventArgs e)
+        {
+            MessageBox.Show("AppointmentEditing");
+        }
+    }
+}
