245662346a0c1f428c565b4c459c3d0b3de20c9a
曾璐 <865514198@qq.com>
2018-09-21 16:51:45 +0800

    wpf command

---
+++ WPF/WpfDemo/Command/ViewModels/CommandDemoWithBindParametersViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfDemo.Command.Commands;
+
+namespace WpfDemo.Command.ViewModels
+{
+    public class CommandDemoWithBindParametersViewModel
+    {
+        public Person Person { get; set; }
+        public ICommand SubmitCommand => _submitCommand;
+        private DelegateCommand _submitCommand = new DelegateCommand(new Action<object>(ShowMessage));
+        private void ShowMessage(object obj)
+        {
+            MessageBox.Show($"{Person.Name},{Person.Age}");
+        }
+    }
+}
---
+++ WPF/WpfDemo/Command/ViewModels/CommandDemoWithParametersViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfDemo.Command.Commands;
+
+namespace WpfDemo.Command.ViewModels
+{
+    public class CommandDemoWithParametersViewModel
+    {
+        public ICommand SubmitCommand => _submitCommand;
+        private RelayCommand _submitCommand = new RelayCommand(new Action<object>(ShowMessage));
+        private static void ShowMessage(object obj)
+        {
+            MessageBox.Show(obj.ToString());
+        }
+    }
+}
