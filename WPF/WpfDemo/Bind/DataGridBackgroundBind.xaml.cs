b7f1afc6bcbf1a15e16963746bb8d20739b619aa
曾璐 <865514198@qq.com>
2018-09-25 17:30:07 +0800

    DataGridBackgroundBind 20%

---
+++ WPF/WpfDemo/Bind/DataGridBackgroundBind.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDemo.Bind.Models;
+
+namespace WpfDemo.Bind
+{
+    /// <summary>
+    /// Interaction logic for DataGridBackgroundBind.xaml
+    /// </summary>
+    public partial class DataGridBackgroundBind : Window
+    {
+        List<Report> reports;
+        public DataGridBackgroundBind()
+        {
+            InitializeComponent();
+            SetData();
+            GenerateDataGrid();
+        }
+
+        private void GenerateDataGrid()
+        {
+            DataGrid dataGrid = new DataGrid();
+            dataGrid.AutoGenerateColumns = false;
+            DataGridTextColumn dataGridTextColumn = new DataGridTextColumn();
+            dataGridTextColumn.Binding = new Binding("ReportDetails.EmployeeName");
+            dataGrid.Columns.Add(dataGridTextColumn);
+            dataGrid.ItemsSource = reports;
+            MyGrid.Children.Add(dataGrid);
+        }
+
+        private void SetData()
+        {
+            reports = new List<Report>();
+            reports.Add(new Report()
+            {
+                StatisticalDate = "2018/01",
+                ReportDetails = new List<ReportDetail>()
+                {
+                    new ReportDetail(){ EmployeeName="李胜",Data=(decimal)1.0},
+                    new ReportDetail(){ EmployeeName="杨军",Data=(decimal)1.1},
+                    new ReportDetail(){ EmployeeName="李琪",Data=(decimal)0.9},
+                    new ReportDetail(){ EmployeeName="刘宇",Data=(decimal)1.2},
+                    new ReportDetail(){ EmployeeName="张屹",Data=(decimal)1.5},
+                }
+            });
+            reports.Add(new Report()
+            {
+                StatisticalDate = "2018/02",
+                ReportDetails = new List<ReportDetail>()
+                {
+                    new ReportDetail(){ EmployeeName="李胜",Data=(decimal)0.9},
+                    new ReportDetail(){ EmployeeName="杨军",Data=(decimal)0.8},
+                    new ReportDetail(){ EmployeeName="李琪",Data=(decimal)1.3},
+                    new ReportDetail(){ EmployeeName="刘宇",Data=(decimal)1.2},
+                    new ReportDetail(){ EmployeeName="张屹",Data=(decimal)1.0},
+                }
+            });
+            reports.Add(new Report()
+            {
+                StatisticalDate = "2018/03",
+                ReportDetails = new List<ReportDetail>()
+                {
+                    new ReportDetail(){ EmployeeName="李胜",Data=(decimal)0.9},
+                    new ReportDetail(){ EmployeeName="杨军",Data=(decimal)0.7},
+                    new ReportDetail(){ EmployeeName="李琪",Data=(decimal)0.6},
+                    new ReportDetail(){ EmployeeName="刘宇",Data=(decimal)1.0},
+                    new ReportDetail(){ EmployeeName="张屹",Data=(decimal)1.1},
+                }
+            });
+        }
+    }
+}
---
+++ WPF/WpfDemo/Bind/Models/Report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.Bind.Models
+{
+    public class Report
+    {
+        /// <summary>
+        /// 统计时间
+        /// </summary>
+        public string StatisticalDate { get; set; }
+        public List<ReportDetail> ReportDetails { get; set; }
+    }
+}
