29d4d36413cb994c3afdbedb6c532b3f5b03b288
曾璐 <865514198@qq.com>
2018-09-27 11:07:06 +0800

    准备迁库

---
+++ Console/CodeLibrary/UsePostgresql/TestDbContext.cs
@@ -0,0 +1,31 @@
+﻿using CodeLibrary.UsePostgresql.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLibrary.UsePostgresql
+{
+    public class TestDbContext : DbContext
+    {
+        public IDbSet<Cat> Cats { get; set; }
+
+        public TestDbContext() : base("Default")
+        {
+
+        }
+
+        public TestDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Console/CodeLibraryForDotNetCore/UsePostgresql/Models/Dog.cs
@@ -0,0 +1,14 @@
+﻿using CodeLibraryForDotNetCore.UsePostgresql.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeLibraryForDotNetCore.UsePostgresql.Models
+{
+    public class Dog
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public SexEnum Sex { get; set; }
+    }
+}
