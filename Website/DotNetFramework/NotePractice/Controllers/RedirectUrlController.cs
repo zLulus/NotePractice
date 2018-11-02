2d2ceacb3d044778aee7955b33c093f8cc8d204c
曾璐 <865514198@qq.com>
2018-11-02 14:52:51 +0800

    RedirectUrl

---
+++ Website/DotNetFramework/NotePractice/Controllers/RedirectUrlController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotePractice.Controllers
+{
+    public class RedirectUrlController : Controller
+    {
+        // GET: RedirectUrl
+        public ActionResult RedirectByForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RedirectByForm(string url)
+        {
+            return Redirect(url);
+        }
+
+        public ActionResult RedirectByAjax()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RedirectByAjax(string url)
+        {
+            return Json(url);
+        }
+    }
+}
