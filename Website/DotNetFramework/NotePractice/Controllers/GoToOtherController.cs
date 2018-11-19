debf24f1f8b8afdf9e96f317d7bc6150f6153f96
曾璐 <865514198@qq.com>
2018-11-19 11:30:09 +0800

    带参跳转其他controller

---
+++ Website/DotNetFramework/NotePractice/Controllers/GoToOtherController.cs
@@ -0,0 +1,23 @@
+﻿using NotePractice.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotePractice.Controllers
+{
+    public class GoToOtherController : Controller
+    {
+        // GET: GoToOther
+        public ActionResult Index()
+        {
+            var vm = new GetValueFromOtherViewModel
+            {
+                Value="123"
+            };
+            TempData["GetValueFromOther"] = vm;
+            return RedirectToAction("Index", "GetValueFromOther");
+        }
+    }
+}
---
+++ Website/DotNetFramework/NotePractice/ViewModels/GetValueFromOtherViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotePractice.ViewModels
+{
+    public class GetValueFromOtherViewModel
+    {
+        public string Value { get; set; }
+    }
+}
