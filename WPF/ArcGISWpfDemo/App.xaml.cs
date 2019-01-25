48faffa7481634075eed0704bbe895b8f1afb1d6
曾璐 <865514198@qq.com>
2019-01-25 14:54:06 +0800

    arcgis

---
+++ WPF/ArcGISWpfDemo/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ArcGISWpfDemo
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.Initialize();
+            }
+            catch (Exception ex)
+            {
+                // Show the message and shut down
+                MessageBox.Show(string.Format("There was an error that prevented initializing the runtime. {0}", ex.Message));
+                Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ WPF/ArcGISWpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArcGISWpfDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Create the UI, setup the control references and execute initialization 
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Create new Map with basemap
+            Map myMap = new Map(Basemap.CreateTerrainWithLabels());
+
+            // Create and set initial map location
+            MapPoint initialLocation = new MapPoint(
+                -13176752, 4090404, SpatialReferences.WebMercator);
+            myMap.InitialViewpoint = new Viewpoint(initialLocation, 300000);
+
+            // Create uri to the used feature service
+            Uri serviceUri = new Uri(
+                "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer/9");
+
+            // Create new FeatureLayer from service uri and
+            FeatureLayer geologyLayer = new FeatureLayer(serviceUri);
+
+            // Add created layer to the map
+            myMap.OperationalLayers.Add(geologyLayer);
+
+            // Assign the map to the MapView
+            MyMapView.Map = myMap;
+        }
+    }
+}
