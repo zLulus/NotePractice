e54064056a2e4273131af03c17409d35a337a8f7
Lulus <865514198@qq.com>
2019-02-16 12:08:03 +0800

    create sql server dbcontext,准备迁库

---
+++ Console/CodeLibraryForDotNetCore/SqlServerUseGeometry/Models/City.cs
@@ -0,0 +1,18 @@
+﻿using GeoAPI.Geometries;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CodeLibraryForDotNetCore.SqlServerUseGeometry.Models
+{
+    [Table("Cities")]
+    public class City
+    {
+        public int CityID { get; set; }
+
+        public string CityName { get; set; }
+
+        public IPoint Location { get; set; }
+    }
+}
---
+++ Console/CodeLibraryForDotNetCore/SqlServerUseGeometry/SqlServerUseGeometryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CodeLibraryForDotNetCore.SqlServerUseGeometry.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeLibraryForDotNetCore.SqlServerUseGeometry
+{
+    public class SqlServerUseGeometryDbContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public SqlServerUseGeometryDbContext(DbContextOptions<SqlServerUseGeometryDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
