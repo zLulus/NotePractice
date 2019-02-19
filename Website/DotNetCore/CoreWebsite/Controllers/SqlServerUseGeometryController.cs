5f576e9a067e80f96229ee9abf9092f886ab4bc3
Lulus <865514198@qq.com>
2019-02-19 10:53:22 +0800

    空间数据库引用

---
+++ Website/DotNetCore/CoreWebsite/Controllers/SqlServerUseGeometryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.Controllers
+{
+    public class SqlServerUseGeometryController : Controller
+    {
+        //参考资料
+        //https://docs.microsoft.com/zh-cn/ef/core/modeling/spatial
+        //https://docs.microsoft.com/zh-cn/sql/t-sql/spatial-geometry/spatial-types-geometry-transact-sql?view=sql-server-2017
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
