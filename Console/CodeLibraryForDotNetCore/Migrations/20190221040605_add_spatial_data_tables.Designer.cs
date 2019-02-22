682a5e16d7c1e7fef19a8aaf8351777fcd49e420
Lulus <865514198@qq.com>
2019-02-22 11:21:01 +0800

    迁库

--- Console/CodeLibraryForDotNetCore/Migrations/20190221040605_add_spatial_data_tables.Designer.cs
+++ Console/CodeLibraryForDotNetCore/Migrations/20190221040605_add_spatial_data_tables.Designer.cs
@@ -2,6 +2,7 @@
 using CodeLibraryForDotNetCore.UsePostgresql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CodeLibraryForDotNetCore.Migrations
 {
     [DbContext(typeof(TestDbContext))]
-    partial class TestDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190221040605_add_spatial_data_tables")]
+    partial class add_spatial_data_tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
