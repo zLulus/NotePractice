376c53afe1a93a5d4dfd5baaeb3c94dfe7988ec2
Lulus <865514198@qq.com>
2019-04-05 10:15:07 +0800

    更新包版本，添加image-compressor.js

--- Website/DotNetCore/CoreNgAlain/src/app/routes/file-upload/file-upload-defaullt/file-upload-defaullt.component.ts
+++ Website/DotNetCore/CoreNgAlain/src/app/routes/file-upload/file-upload-defaullt/file-upload-defaullt.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UploadFile,NzNotificationService } from 'ng-zorro-antd';
+import { UploadFile, NzNotificationService } from 'ng-zorro-antd';
 // import { Config } from '@assets/Config';
 
 @Component({
@@ -29,55 +29,55 @@ export class FileUploadDefaulltComponent implements OnInit {
 
   }
 
-  //图片预览
-  handlePreview = (file: UploadFile) => {
-    this.previewImage = file.url || file.thumbUrl;
-    this.previewVisible = true;
-  };
+  // //图片预览
+  // handlePreview = (file: UploadFile) => {
+  //   this.previewImage = file.url || file.thumbUrl;
+  //   this.previewVisible = true;
+  // };
 
-  //图片上传前
-  beforeUpload = async (item) => {
-    try {
-      //这里赋值参数item无意义，没有改变实际上传的图片对象，所以不能在这里完成图片压缩
-      return true;
-    } catch (e) {
-        return false;
-    }
-  }
+  // //图片上传前
+  // beforeUpload = async (item) => {
+  //   try {
+  //     //这里赋值参数item无意义，没有改变实际上传的图片对象，所以不能在这里完成图片压缩
+  //     return true;
+  //   } catch (e) {
+  //       return false;
+  //   }
+  // }
 
-  //图片上传返回
-  handleChange(info: { file: UploadFile }): void {
-    if (info.file.status === 'error') {
-      this.notification.create('error','error','上传图片异常，请重试');
-    }
-    if (info.file.status === 'done') {
-      this.getBase64(info.file.originFileObj, (img: any) => {
-        //获得图片的base64
-      });
-      this.notification.create('success','success','上传图片完成');
-    }
-  }
-  private getBase64(img: File, callback: (img: any) => void) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result));
-    reader.readAsDataURL(img);
-  }
+  // //图片上传返回
+  // handleChange(info: { file: UploadFile }): void {
+  //   if (info.file.status === 'error') {
+  //     this.notification.create('error','error','上传图片异常，请重试');
+  //   }
+  //   if (info.file.status === 'done') {
+  //     this.getBase64(info.file.originFileObj, (img: any) => {
+  //       //获得图片的base64
+  //     });
+  //     this.notification.create('success','success','上传图片完成');
+  //   }
+  // }
+  // private getBase64(img: File, callback: (img: any) => void) {
+  //   const reader = new FileReader();
+  //   reader.addEventListener('load', () => callback(reader.result));
+  //   reader.readAsDataURL(img);
+  // }
 
-  //删除图片
-  onRemove = (file: UploadFile) => {
-    const currIndex = this.findImgIndex(file);
-    delete this.fileList[currIndex];
-    return true;
-  }
-  private findImgIndex(url) {
-    let retIndex = -1;
-    this.fileList.forEach((item, index) => {
-        if (item.thumbUrl === url || item.url === url) retIndex = index;
-    });
-    return retIndex;
-  }
+  // //删除图片
+  // onRemove = (file: UploadFile) => {
+  //   const currIndex = this.findImgIndex(file);
+  //   delete this.fileList[currIndex];
+  //   return true;
+  // }
+  // private findImgIndex(url) {
+  //   let retIndex = -1;
+  //   this.fileList.forEach((item, index) => {
+  //       if (item.thumbUrl === url || item.url === url) retIndex = index;
+  //   });
+  //   return retIndex;
+  // }
 
-  getFileList=function(){
-    console.log(this.fileList);
-  }
+  // getFileList=function(){
+  //   console.log(this.fileList);
+  // }
 }
