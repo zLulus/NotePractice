4b4d9ce6ee596d317282b926d9842a4c1d85711f
Lulus <865514198@qq.com>
2020-03-22 21:30:54 +0800

    BaseAndSubClassTypeConversionDemo

---
+++ Console/CodeLibraryForDotNetCore/TypeConversion/Models/NegativePlant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeLibraryForDotNetCore.TypeConversion.Models
+{
+    /// <summary>
+    /// 阴性植物
+    /// </summary>
+    public class NegativePlant: Plant
+    {
+        public decimal MaximumSurvivalTemperature { get; set; }
+    }
+}
---
+++ Console/CodeLibraryForDotNetCore/TypeConversion/Models/PositivePlant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeLibraryForDotNetCore.TypeConversion.Models
+{
+    /// <summary>
+    /// 阳性植物
+    /// </summary>
+    public class PositivePlant: Plant
+    {
+        public decimal MinimumSurvivalTemperature { get; set; }
+    }
+}
