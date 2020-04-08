d6f727547c982f18ba4df4639a50736579e7d1c8
865514198@qq.com <aizaiyinghua555>
2020-04-08 17:29:40 +0800

    arcgis

---
+++ WPF/ArcGISNewWpfDemo/MapViewModel.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Security;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.UI;
+
+namespace ArcGISNewWpfDemo
+{
+    /// <summary>
+    /// Provides map data to an application
+    /// </summary>
+    public class MapViewModel : INotifyPropertyChanged
+    {
+        public MapViewModel()
+        {
+
+        }
+
+        private Map _map = new Map(Basemap.CreateStreetsVector());
+
+        /// <summary>
+        /// Gets or sets the map
+        /// </summary>
+        public Map Map
+        {
+            get { return _map; }
+            set { _map = value; OnPropertyChanged(); }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="MapViewModel.PropertyChanged" /> event
+        /// </summary>
+        /// <param name="propertyName">The name of the property that has changed</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var propertyChangedHandler = PropertyChanged;
+            if (propertyChangedHandler != null)
+                propertyChangedHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
