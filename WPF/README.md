d2b017622d4de31c93e4acf1993917bf69039143
865514198@qq.com <aizaiyinghua555>
2020-04-15 17:23:04 +0800

    MoveAndResizeControl

--- WPF/README.md
+++ WPF/README.md
@@ -8,3 +8,4 @@ WpfDemo：Demo based on WPF basic knowledge.Divide folders according to function
 ArcGISWpfDemo：基于ArcGIS for Wpf的demo    
 CefSharpWpfDemo： 基于CefSharp for Wpf的demo    
 WpfDemo： 写基于WPF的基本知识，按照功能点分文件夹，通过修改App.xaml的StartupUri更改启动页面     
+SuperMapWpfDemo：  基于SuperMap iObjects .NET的demo 
---
+++ WPF/WpfDemo/MoveAndResizeControl/Tools/Element.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfDemo.MoveAndResizeControl.Tools
+{
+    public class Element
+    {
+        #region Fields
+        private bool isDragging = false;
+        private bool isStretching = false;
+        private bool stretchLeft = false;
+        private bool stretchRight = false;
+        private IInputElement inputElement = null;
+        private double x, y = 0;
+        private int zIndex = 0;
+        #endregion
+
+        #region Constructor
+        public Element() { }
+        #endregion
+
+        #region Properties
+        public IInputElement InputElement
+        {
+            get { return this.inputElement; }
+            set
+            {
+                this.inputElement = value;
+                this.isDragging = false;
+                this.isStretching = false;
+            }
+        }
+        public double X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+        public double Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+        public int ZIndex
+        {
+            get { return this.zIndex; }
+            set { this.zIndex = value; }
+        }
+        public bool IsDragging
+        {
+            get { return this.isDragging; }
+            set
+            {
+                this.isDragging = value;
+                this.isStretching = !this.isDragging;
+            }
+        }
+        public bool IsStretching
+        {
+            get { return this.isStretching; }
+            set
+            {
+                this.isStretching = value;
+                this.IsDragging = !this.isStretching;
+            }
+        }
+        public bool StretchLeft
+        {
+            get { return this.stretchLeft; }
+            set { this.stretchLeft = value; this.stretchRight = !this.stretchLeft; }
+        }
+        public bool StretchRight
+        {
+            get { return this.stretchRight; }
+            set { this.stretchRight = value; this.stretchLeft = !this.stretchRight; }
+        }
+        #endregion
+    }
+}
