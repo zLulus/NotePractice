77631e5f1751642f64d6368f28017efee2702229
865514198@qq.com <aizaiyinghua555>
2020-04-22 10:18:50 +0800

    ResizeImageAndPrint

--- WPF/WpfDemo/PrintImage/PrintImageWindow.xaml.cs
+++ WPF/WpfDemo/PrintImage/PrintImageWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -61,11 +64,40 @@ namespace WpfDemo.PrintImage
             try
             {
                 this.Cursor = Cursors.Wait;
+
+                //直接打印
+                //DirectPrint(outputPath);
+                //根据打印设置重新计算图片高宽后打印
+                ResizeImageAndPrint(outputPath);
+            }
+            finally
+            {
+                this.Cursor = Cursors.AppStarting;
+            }
+        }
+
+        /// <summary>
+        /// 根据打印设置重新计算图片高宽后打印
+        /// </summary>
+        /// <param name="outputPath"></param>
+        private void ResizeImageAndPrint(string outputPath)
+        {
+            var pdialog = new PrintDialog();
+            if (pdialog.ShowDialog() == true)
+            {
+                //根据纸张大小和纸张方向，读取图片，修改图片大小
+                var height = pdialog.PrintableAreaHeight;
+                var width = pdialog.PrintableAreaWidth;
+
+                Bitmap bmp = new Bitmap(outputPath);
+                var newImage = RezizeImage(bmp, (int)width, (int)height);
+                var newPath = $"{outputDic}\\{Guid.NewGuid().ToString()}.png";
+                newImage.Save(newPath, ImageFormat.Png);
+
                 var bi = new BitmapImage();
                 bi.BeginInit();
                 bi.CacheOption = BitmapCacheOption.OnLoad;
-                //加载图片
-                bi.UriSource = new Uri(outputPath);
+                bi.UriSource = new Uri(newPath);
                 bi.EndInit();
 
                 var vis = new DrawingVisual();
@@ -74,16 +106,65 @@ namespace WpfDemo.PrintImage
                     dc.DrawImage(bi, new Rect { Width = bi.Width, Height = bi.Height });
                 }
 
-                var pdialog = new PrintDialog();
-                if (pdialog.ShowDialog() == true)
-                {
-                    pdialog.PrintVisual(vis, "My Image");
-                }
+                pdialog.PrintVisual(vis, "My Image");
+
+                File.Delete(newPath);
             }
-            finally
+        }
+
+        /// <summary>
+        /// 直接打印
+        /// </summary>
+        /// <param name="outputPath"></param>
+        private static void DirectPrint(string outputPath)
+        {
+            var bi = new BitmapImage();
+            bi.BeginInit();
+            bi.CacheOption = BitmapCacheOption.OnLoad;
+            //加载图片
+            bi.UriSource = new Uri(outputPath);
+            bi.EndInit();
+
+            var vis = new DrawingVisual();
+            using (var dc = vis.RenderOpen())
             {
-                this.Cursor = Cursors.AppStarting;
+                dc.DrawImage(bi, new Rect { Width = bi.Width, Height = bi.Height });
             }
+
+            var pdialog = new PrintDialog();
+            if (pdialog.ShowDialog() == true)
+            {
+                pdialog.PrintVisual(vis, "My Image");
+            }
+        }
+
+        //https://stackoverflow.com/questions/2319983/resizing-an-image-in-asp-net-without-losing-the-image-quality
+        private System.Drawing.Image RezizeImage(System.Drawing.Image img, int maxWidth, int maxHeight)
+        {
+            if (img.Height < maxHeight && img.Width < maxWidth) return img;
+            using (img)
+            {
+                Double xRatio = (double)img.Width / maxWidth;
+                Double yRatio = (double)img.Height / maxHeight;
+                Double ratio = Math.Max(xRatio, yRatio);
+                int nnx = (int)Math.Floor(img.Width / ratio);
+                int nny = (int)Math.Floor(img.Height / ratio);
+                Bitmap cpy = new Bitmap(nnx, nny, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                using (Graphics gr = Graphics.FromImage(cpy))
+                {
+                    gr.Clear(System.Drawing.Color.Transparent);
+
+                    // This is said to give best quality when resizing images
+                    gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+                    gr.DrawImage(img,
+                        new System.Drawing.Rectangle(0, 0, nnx, nny),
+                        new System.Drawing.Rectangle(0, 0, img.Width, img.Height),
+                        GraphicsUnit.Pixel);
+                }
+                return cpy;
+            }
+
         }
     }
 }
