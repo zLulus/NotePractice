f3f31e79b0b67817c681ad4d1caa020674031ca6
zenglu <865514198@qq.com>
2020-05-15 10:02:12 +0800

    CustomButton

---
+++ WPF/WpfDemo/CustomButton/XButton.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDemo.CustomButton
+{
+    /// <summary>
+    /// XButton.xaml 的交互逻辑
+    /// </summary>
+    public partial class XButton : Button
+    {
+        public XButton()
+        {
+            InitializeComponent();
+            ToolTip = "关闭";
+        }
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+            Window.GetWindow(this).Close();
+        }
+    }
+}
