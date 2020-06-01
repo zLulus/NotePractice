9f840fde2b2454d8b7e1c1f5e9f071aacfc7483d
zenglu <865514198@qq.com>
2020-06-01 13:20:58 +0800

    SingleSelectors

---
+++ WPF/WpfDemo/SingleSelectors/README.md
@@ -0,0 +1,5 @@
+﻿TreeView+RadioButton的用户控件     
+参考资料：[Using Radio Button in Tree View in WPF](https://zamjad.wordpress.com/2009/10/04/using-radio-button-in-tree-view-in-wpf/)
+
+SingleSelectorWindow:前端生成   
+SingleSelectorWindow2:后端生成    
---
+++ WPF/WpfDemo/SingleSelectors/SingleSelectorWindow2.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDemo.SingleSelectors.ViewModels;
+
+namespace WpfDemo.SingleSelectors
+{
+    /// <summary>
+    /// SingleSelectorWindow2.xaml 的交互逻辑
+    /// </summary>
+    public partial class SingleSelectorWindow2 : Window
+    {
+        SingleSelector singleSelector;
+        public SingleSelectorWindow2()
+        {
+            InitializeComponent();
+
+            //模拟数据
+            List<SingleSelectorViewModel> data = new List<SingleSelectorViewModel>();
+            var first = new SingleSelectorViewModel() { Id = 1, ParendId = 0, Name = "湖南省" };
+            first.Chidlren = new List<SingleSelectorViewModel>();
+            var child1 = new SingleSelectorViewModel() { Id = 11, ParendId = 1, Name = "长沙市" };
+            child1.Chidlren = new List<SingleSelectorViewModel>();
+            child1.Chidlren.Add(new SingleSelectorViewModel() { Id = 111, ParendId = 11, Name = "芙蓉区" });
+            child1.Chidlren.Add(new SingleSelectorViewModel() { Id = 112, ParendId = 11, Name = "天心区" });
+            first.Chidlren.Add(child1);
+            first.Chidlren.Add(new SingleSelectorViewModel() { Id = 12, ParendId = 1, Name = "株洲市" });
+            data.Add(first);
+            var second = new SingleSelectorViewModel() { Id = 2, ParendId = 0, Name = "北京市" };
+            data.Add(second);
+            //创建树控件
+            singleSelector = new SingleSelector(data);
+            border.Child = singleSelector;
+        }
+
+        private void btnSelectId_Click(object sender, RoutedEventArgs e)
+        {
+            var sel = singleSelector.GetSelectItem();
+            if (sel != null)
+            {
+                var d = sel as SingleSelectorViewModel;
+                MessageBox.Show($"选中项：{d.Id},{d.Name}");
+            }
+            else
+            {
+                MessageBox.Show($"没有选中项");
+            }
+        }
+    }
+}
