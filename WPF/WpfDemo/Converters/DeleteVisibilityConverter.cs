b6d4cce1982eb9cae097123f722dfed532a9f736
zenglu <865514198@qq.com>
2020-06-03 11:00:50 +0800

    DataGrid with Converter

---
+++ WPF/WpfDemo/Converters/DeleteVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfDemo.Converters
+{
+    public class DeleteVisibilityConverter : IValueConverter
+    {
+        //当值从绑定源传播给绑定目标时，调用方法Convert
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var data = value as FakeDatabase;
+            if (data.Id % 2 == 0)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+        //当值从绑定目标传播给绑定源时，调用此方法ConvertBack
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new Exception("");
+        }
+    }
+}
