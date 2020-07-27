202272526b3421bf3b26d73edb620f0722b236fb
zenglu <865514198@qq.com>
2020-07-27 14:52:36 +0800

    ClosableTabs

---
+++ WPF/WpfDemo/ClosableTabs/ClosableTabDemo.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDemo.SingleSelectors;
+
+namespace WpfDemo.ClosableTabs
+{
+    /// <summary>
+    /// ClosableTabDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class ClosableTabDemo : Window
+    {
+        public ClosableTabDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            ClosableTab theTabItem = new ClosableTab();
+            theTabItem.Title = "Small title";
+            tabControl1.Items.Add(theTabItem);
+            theTabItem.Focus();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ClosableTab theTabItem = new ClosableTab();
+            theTabItem.Title = "A much longer title for an example";
+            tabControl1.Items.Add(theTabItem);
+            theTabItem.Focus();
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            ClosableTab theTabItem = new ClosableTab();
+            theTabItem.Content = new SingleSelector();
+            theTabItem.Title = "Single Selector";
+            tabControl1.Items.Add(theTabItem);
+            theTabItem.Focus();
+        }
+    }
+}
