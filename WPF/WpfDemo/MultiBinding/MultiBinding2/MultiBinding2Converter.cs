5ac4881e235414555169bfc845670d0d6cfb3996
zenglu <865514198@qq.com>
2020-09-07 13:23:43 +0800

    MultiBinding2

---
+++ WPF/WpfDemo/MultiBinding/MultiBinding2/MultiBinding2Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfDemo.MultiBinding.MultiBinding2
+{
+    public class MultiBinding2Converter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var selectItem = values[0] as ComboBoxItem;
+            var isChecked = values[1] as bool?;
+            //此处不需要该参数，这里只是展示Converter传参一个固定字符串
+            var para = parameter as string;
+            if(selectItem!=null && selectItem.Tag.ToString()=="1" &&
+                isChecked.HasValue && isChecked.Value)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ WPF/WpfDemo/MultiBinding/MultiBinding2/README.md
@@ -0,0 +1,4 @@
+﻿MultiBinding多参数+Converter+ConverterParameter
+
+#### 参考资料
+[Binding ConverterParameter](https://stackoverflow.com/questions/15309008/binding-converterparameter)
