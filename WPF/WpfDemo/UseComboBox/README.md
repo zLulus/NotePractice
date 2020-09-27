86d01cc0c8711cfef521ad6f605a5d55e8949542
zenglu <865514198@qq.com>
2020-09-27 17:39:19 +0800

    ComboBox

---
+++ WPF/WpfDemo/UseComboBox/README.md
@@ -0,0 +1,4 @@
+﻿ComboBox绑定自定义数据源，并设置好显示字段、选择项
+
+数据源及显示字段，设置好`ItemsSource`和`DisplayMemberPath`
+获得选择项，设置
---
+++ WPF/WpfDemo/UseComboBox/UseComboBoxDemo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfDemo.UseComboBox.ViewModels;
+
+namespace WpfDemo.UseComboBox
+{
+    /// <summary>
+    /// UseComboBoxDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class UseComboBoxDemo : UserControl
+    {
+        UseComboBoxViewModel vm { get; set; }
+        public UseComboBoxDemo()
+        {
+            InitializeComponent();
+
+            vm = new UseComboBoxViewModel() { Fruits = new ObservableCollection<FruitViewModel>() };
+            vm.Fruits.Add(new FruitViewModel() { Id = 1, Name = "Apple" });
+            vm.Fruits.Add(new FruitViewModel() { Id = 2, Name = "Pear" });
+            vm.Fruits.Add(new FruitViewModel() { Id = 3, Name = "Banana" });
+            DataContext = vm;
+        }
+    }
+}
