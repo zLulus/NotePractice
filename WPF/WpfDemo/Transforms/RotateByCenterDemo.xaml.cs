f3280e537dddf3a1b66b74a19ce6ad3b47029b9c
曾璐 <865514198@qq.com>
2020-11-27 13:30:56 +0800

    Rotate By Center Demo

---
+++ WPF/WpfDemo/Transforms/RotateByCenterDemo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDemo.Transforms
+{
+    /// <summary>
+    /// Interaction logic for RotateByCenterDemo.xaml
+    /// </summary>
+    public partial class RotateByCenterDemo : UserControl
+    {
+        public RotateByCenterDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
