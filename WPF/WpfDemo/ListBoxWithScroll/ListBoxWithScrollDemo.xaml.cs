70d01869b5d18835185e0f8b847e7285886fe2dc
曾璐 <865514198@qq.com>
2020-12-10 10:18:15 +0800

    ListBox With Scroll

---
+++ WPF/WpfDemo/ListBoxWithScroll/ListBoxWithScrollDemo.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDemo.ListBoxWithScroll
+{
+    /// <summary>
+    /// Interaction logic for ListBoxWithScrollDemo.xaml
+    /// </summary>
+    public partial class ListBoxWithScrollDemo : UserControl
+    {
+        public ListBoxWithScrollDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0; i <= 500; i++)
+            {
+                listBox.Items.Add(new ListBoxItem() { Content = i });
+            }
+        }
+
+        private void MoveToButtom_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.Items.Count > 0)
+            {
+                var last = listBox.Items[listBox.Items.Count - 1];
+                listBox.ScrollIntoView(last);
+            }
+            
+        }
+    }
+}
