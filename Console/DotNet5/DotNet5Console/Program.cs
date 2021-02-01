b9adfd513ba8f4044171abb8d542ab4b0d85173c
曾璐 <865514198@qq.com>
2021-02-01 10:06:34 +0800

    DotNet5 Console

---
+++ Console/DotNet5/DotNet5Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet5Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<string> values = new();
+
+            Console.ReadLine();
+        }
+    }
+}
