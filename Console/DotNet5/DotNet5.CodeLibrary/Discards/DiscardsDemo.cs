b935c2c5f80215eff70ea4dcf405f3ce1fe0a596
曾璐 <865514198@qq.com>
2021-02-01 14:14:41 +0800

    Discards

---
+++ Console/DotNet5/DotNet5.CodeLibrary/Discards/DiscardsDemo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet5.CodeLibrary.Discards
+{
+    public class DiscardsDemo
+    {
+        public static async Task Run()
+        {
+            //使用弃元：使用独立占位符来忽略异步操作返回的 Task 对象，抑制操作即将完成时所引发的异常
+            await ExecuteAsyncMethodsWithDiscards();
+            //不使用弃元：抛出异常
+            //await ExecuteAsyncMethodsWithoutDiscards();
+        }
+
+        public static async Task ExecuteAsyncMethodsWithDiscards()
+        {
+            Console.WriteLine("About to launch a task...");
+            _ = Task.Run(() => {
+                var iterations = 0;
+                for (int ctr = 0; ctr < int.MaxValue; ctr++)
+                    iterations++;
+                Console.WriteLine("Completed looping operation...");
+                throw new InvalidOperationException();
+            });
+            await Task.Delay(5000);
+            Console.WriteLine("Exiting after 5 second delay");
+        }
+
+        public static async Task ExecuteAsyncMethodsWithoutDiscards()
+        {
+            Console.WriteLine("About to launch a task...");
+            await Task.Run(() => {
+                var iterations = 0;
+                for (int ctr = 0; ctr < int.MaxValue; ctr++)
+                    iterations++;
+                Console.WriteLine("Completed looping operation...");
+                throw new InvalidOperationException();
+            });
+            await Task.Delay(5000);
+            Console.WriteLine("Exiting after 5 second delay");
+        }
+    }
+}
--- Console/DotNet5/DotNet5Console/Program.cs
+++ Console/DotNet5/DotNet5Console/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using DotNet5.CodeLibrary.Discards;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DotNet5Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            List<string> values = new();
+            //弃元
+            //https://docs.microsoft.com/zh-cn/dotnet/csharp/discards?WT.mc_id=DT-MVP-5003010
+            await DiscardsDemo.Run();
 
             Console.ReadLine();
         }
