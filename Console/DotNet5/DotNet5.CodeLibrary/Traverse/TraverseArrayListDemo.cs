a9c789d8b810b489e6af3f9dfc7a73a771801b44
lulu <865514198@qq.com>
2021-04-15 11:24:08 +0800

    Traverse

---
+++ Console/DotNet5/DotNet5.CodeLibrary/Traverse/TraverseArrayListDemo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet5.CodeLibrary.Traverse
+{
+    public class TraverseArrayListDemo
+    {
+        public static void Run()
+        {
+            //去掉集合中的所有奇数
+
+            //正序
+            RemoveAtWithOrder();
+            //倒序
+            RemoveAtWithReverseOrder();
+        }
+
+        /// <summary>
+        /// 随机100W个数字
+        /// </summary>
+        /// <returns></returns>
+        public static ArrayList GetRandomDatas()
+        {
+            ArrayList datas = new ArrayList(1000000);
+            for (int i = 0; i < 1000000; i++)
+            {
+                Random random = new Random();
+                datas.Add(random.Next(100000));
+            }
+
+            return datas;
+        }
+
+        private static void RemoveAtWithOrder()
+        {
+            ArrayList datas = GetRandomDatas();
+
+            var start = Environment.TickCount;
+            for (int i = 0; i < datas.Count;)
+            {
+                if ((int)datas[i] % 2 != 0)
+                    datas.RemoveAt(i);
+                else
+                    i++;
+            }
+            var end = Environment.TickCount;
+            var t = end - start;
+            Console.WriteLine($"正序用时:{t}");
+        }
+
+
+        private static void RemoveAtWithReverseOrder()
+        {
+            ArrayList datas = GetRandomDatas();
+
+            var start = Environment.TickCount;
+            for (int i = datas.Count - 1; i >= 0; i--)
+            {
+                if ((int)datas[i] % 2 != 0)
+                    datas.RemoveAt(i);
+            }
+            var end = Environment.TickCount;
+            var t = end - start;
+            Console.WriteLine($"倒序用时:{t}");
+        }
+    }
+}
--- Console/DotNet5/DotNet5Console/Program.cs
+++ Console/DotNet5/DotNet5Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DotNet5.CodeLibrary.ArraySegments;
 using DotNet5.CodeLibrary.Discards;
+using DotNet5.CodeLibrary.Traverse;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -14,7 +15,12 @@ namespace DotNet5Console
             //https://docs.microsoft.com/zh-cn/dotnet/csharp/discards?WT.mc_id=DT-MVP-5003010
             //await DiscardsDemo.Run();
 
-            await ArraySegmentDemo.Run();
+            //await ArraySegmentDemo.Run();
+
+            //Console.WriteLine("List集合正序/倒序操作:");
+            //TraverseListDemo.Run();
+            Console.WriteLine("ArrayList集合正序/倒序操作:");
+            TraverseArrayListDemo.Run();
 
             Console.ReadLine();
         }
