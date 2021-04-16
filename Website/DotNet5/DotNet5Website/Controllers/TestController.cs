efb8db953cbae7709bb611b7f48f99618a47450b
lulu <865514198@qq.com>
2021-04-16 14:46:14 +0800

    test api

---
+++ Website/DotNet5/DotNet5Website/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5Website.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
