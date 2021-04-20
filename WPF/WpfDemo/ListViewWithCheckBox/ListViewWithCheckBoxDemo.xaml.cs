2e8d19681427351ed554fe6f9aae6c9e6994a922
lulu <865514198@qq.com>
2021-04-20 14:24:22 +0800

    ListViewWithCheckBoxDemo

---
+++ WPF/WpfDemo/ListViewWithCheckBox/ListViewWithCheckBoxDemo.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfDemo.ListViewWithCheckBox.ViewModels;
+
+namespace WpfDemo.ListViewWithCheckBox
+{
+    /// <summary>
+    /// ListViewWithCheckBoxDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class ListViewWithCheckBoxDemo : UserControl
+    {
+        ObservableCollection<MyTask> vm { get; set; }
+        public ListViewWithCheckBoxDemo()
+        {
+            InitializeComponent();
+
+            vm = new ObservableCollection<MyTask>();
+            vm.Add(new MyTask() { TaskName = "任务1", IsChecked = false });
+            vm.Add(new MyTask() { TaskName = "任务2", IsChecked = true });
+
+            taskListBox.DataContext = vm;
+        }
+
+        private void SelectAll_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach(var data in vm)
+            {
+                data.IsChecked = true;
+            }
+        }
+
+        private void SelectAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (var data in vm)
+            {
+                data.IsChecked = false;
+            }
+        }
+    }
+}
---
+++ WPF/WpfDemo/ListViewWithCheckBox/ViewModels/MyTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.ListViewWithCheckBox.ViewModels
+{
+    public class MyTask : INotifyPropertyChanged
+    {
+        private string taskName;
+        public string TaskName
+        {
+            get
+            {
+                return taskName;
+            }
+            set
+            {
+                taskName = value;
+                NotifyPropertyChanged(nameof(TaskName));
+            }
+        }
+        private bool isChecked;
+        public bool IsChecked
+        {
+            get 
+            { 
+                return isChecked; 
+            }
+            set
+            {
+                isChecked = value;
+                NotifyPropertyChanged(nameof(IsChecked));
+            }
+        }
+
+        /// <summary>
+        /// 属性改变事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
