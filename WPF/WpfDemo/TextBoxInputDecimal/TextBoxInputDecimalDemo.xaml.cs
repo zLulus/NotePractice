377eb77ec7abaabd28655864a8ac023a054b336a
lulu <865514198@qq.com>
2021-04-20 14:55:19 +0800

    TextBoxInputDecimalDemo

---
+++ WPF/WpfDemo/TextBoxInputDecimal/TextBoxInputDecimalDemo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfDemo.TextBoxInputDecimal.ViewModels;
+
+namespace WpfDemo.TextBoxInputDecimal
+{
+    /// <summary>
+    /// TextBoxInputDecimalDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class TextBoxInputDecimalDemo : UserControl
+    {
+        //https://stackoverflow.com/questions/18964518/wpf-converters-with-different-decimal-number
+        TextBoxInputDecimalViewModel vm { get; set; }
+        public TextBoxInputDecimalDemo()
+        {
+            InitializeComponent();
+
+            vm = new TextBoxInputDecimalViewModel();
+            DataContext = vm;
+        }
+
+        private void GetNumber_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"You input: {vm.InputNumber}");
+        }
+    }
+}
---
+++ WPF/WpfDemo/TextBoxInputDecimal/ViewModels/TextBoxInputDecimalViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.TextBoxInputDecimal.ViewModels
+{
+    public class TextBoxInputDecimalViewModel : INotifyPropertyChanged
+    {
+        private decimal inputNumber;
+        public decimal InputNumber
+        {
+            get
+            {
+                return inputNumber;
+            }
+            set
+            {
+                inputNumber = value;
+                NotifyPropertyChanged(nameof(InputNumber));
+            }
+        }
+        /// <summary>
+        /// 属性改变事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
