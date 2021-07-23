f4e4d319d50d30d85cc37c0254b9344883d85583
lulu <865514198@qq.com>
2021-07-23 15:49:35 +0800

    异常处理

--- WPF/WpfDemo/App.xaml.cs
+++ WPF/WpfDemo/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfDemo.Tools;
 
 namespace WpfDemo
 {
@@ -17,14 +18,56 @@ namespace WpfDemo
         {
             //wpf 程序异常捕获，而不崩溃退出
             DispatcherUnhandledException += App_DispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            e.SetObserved();
+            ExceptionHandler(e.Exception.InnerException);
         }
 
-        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            MessageBox.Show("程序异常." + Environment.NewLine + e.Exception.Message);
-            //Shutdown(1);
-            //标记为异常已经处理
             e.Handled = true;
+            ExceptionHandler(e.Exception);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ExceptionHandler((Exception)e.ExceptionObject);
+        }
+
+        private void ExceptionHandler(Exception ex)
+        {
+            Dispatcher.Invoke(() =>
+            {
+#if DEBUG
+                MessageBox.Show(string.Format("调用堆栈：\n{0}\n\n\n异常信息：\n{1}", ex.StackTrace, ex.Message));
+#else
+                MessageBox.Show(string.Format("异常信息：\n{0}",  ex.Message));
+#endif  
+            });
+
+            RecordException(ex,true);
+        }
+
+        private static void RecordException(Exception ex, bool isTop)
+        {
+            var logger = LogTool.GetFileLogger($"异常日志");
+            logger.Info($"Message：{ex.Message}");
+            logger.Info($"ToString：{ex.ToString()}");
+            logger.Info($"Source：{ex.Source}");
+            logger.Info($"StackTrace：{ex.StackTrace}");
+            if (ex.InnerException != null)
+            {
+                RecordException(ex.InnerException, false);
+            }
+            if (isTop)
+            {
+                logger.Info("\n");
+                logger.Info("\n");
+            }
         }
     }
 }
---
+++ WPF/WpfDemo/Tools/LogTool.cs
@@ -0,0 +1,65 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.Tools
+{
+    internal class LogTool
+    {
+        //先初始化Config
+        private static LoggingConfiguration Config = new LoggingConfiguration();
+        private static object LockObject = new object();
+        private static void Configuration(string loggerName)
+        {
+            var consoleTarget = new ConsoleTarget()
+            {
+                Layout = @"${date:format=HH\:mm\:ss} ${message}",
+                Encoding = Encoding.Default
+            };
+            //采用*号命名的LoggingRule，不用显示引用，启动其他任何name的LoggingRule都会自动带上它，默认起效。
+            Config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, consoleTarget));
+            var now = DateTime.Now;
+            var fileTarget = new FileTarget()
+            {
+                //按天分文件夹
+                FileName = @"${basedir}/output/" + $"{now.Year}-{now.Month}-{now.Day}" + "/" + loggerName + ".log",
+                Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss} ${message}",
+                Encoding = Encoding.Default,
+                //日志归档条件，大小为字节
+                //https://stackoverflow.com/questions/50501773/how-to-set-nlog-max-file-size
+                //ArchiveAboveSize
+            };
+            Config.LoggingRules.Add(new LoggingRule(loggerName, LogLevel.Debug, fileTarget));
+            LogManager.Configuration = Config;
+        }
+        public static Logger GetFileLogger(string loggerName)
+        {
+            //Config里面有实例
+            lock (LockObject)
+            {
+                foreach (var item in Config.LoggingRules)
+                {
+                    if (item.LoggerNamePattern == loggerName)
+                    {
+                        Config.LoggingRules.Remove(item);
+                        break;
+                    }
+                }
+                //Config里面没有实例，创建实例后返回
+                Configuration(loggerName);
+            }
+            return LogManager.GetLogger(loggerName);
+        }
+
+        public static void LogDebugInfo(string method, string content, string paramaters)
+        {
+            Logger logger = GetFileLogger("Exception");
+            logger.Debug($"{method}:{content}  paramaters:{paramaters}");
+        }
+    }
+}
