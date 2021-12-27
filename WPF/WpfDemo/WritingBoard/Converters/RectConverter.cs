dd8b294345581f2ca101da15185963869e3c258c
lulu <865514198@qq.com>
2021-12-27 17:49:10 +0800

    WritingBoardDemo

---
+++ WPF/WpfDemo/WritingBoard/Converters/RectConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfDemo.WritingBoard.Converters
+{
+    internal class RectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Rect(0, 0, (double)values[0], (double)values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
