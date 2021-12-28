5bb632e6a26b417f7599e301633d75a2f68aca46
lulu <865514198@qq.com>
2021-12-28 10:00:14 +0800

    SettingWithComboBoxDemo

---
+++ WPF/WpfDemo/UseComboBox/SettingWithComboBoxDemo.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using WpfDemo.UseComboBox.ViewModels;
+
+namespace WpfDemo.UseComboBox
+{
+    /// <summary>
+    /// SettingWithComboBoxDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingWithComboBoxDemo : UserControl
+    {
+        SettingWithComboBoxDemoViewModel vm { get; set; }
+        public SettingWithComboBoxDemo()
+        {
+            InitializeComponent();
+
+            vm = new SettingWithComboBoxDemoViewModel();
+            vm.Fruits = new ObservableCollection<FruitViewModel>();
+            vm.Fruits.Add(new FruitViewModel() { Id = 1, Name = "Apple" });
+            vm.Fruits.Add(new FruitViewModel() { Id = 2, Name = "Pear" });
+            vm.Fruits.Add(new FruitViewModel() { Id = 3, Name = "Banana" });
+            vm.SelectFruit = vm.Fruits.FirstOrDefault(x => x.Id == 1);
+
+            DataContext = vm;
+        }
+
+        private void GetCurrentSelectItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(vm.SelectFruit!=null)
+                MessageBox.Show($"{vm.SelectFruit.Name}");
+            else
+                MessageBox.Show($"None");
+        }
+
+        private void ResetSelectItem_Click(object sender, RoutedEventArgs e)
+        {
+            vm.SelectFruit = null;
+        }
+    }
+}
---
+++ WPF/WpfDemo/UseComboBox/ViewModels/SettingWithComboBoxDemoViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.UseComboBox.ViewModels
+{
+    internal class SettingWithComboBoxDemoViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<FruitViewModel> Fruits { get; set; }
+
+        private FruitViewModel selectFruit;
+        public FruitViewModel SelectFruit
+        {
+            get { return selectFruit; }
+            set
+            {
+                if (selectFruit != value)
+                {
+                    selectFruit = value;
+                    NotifyPropertyChanged(nameof(SelectFruit));
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        /// <summary>
+        /// Need to implement this interface in order to get data binding
+        /// to work properly.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
