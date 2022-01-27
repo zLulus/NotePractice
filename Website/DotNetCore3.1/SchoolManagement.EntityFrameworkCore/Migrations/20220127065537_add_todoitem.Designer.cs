7f7b07fdf48155ffe70f2614b7d50bb056eb4620
lulu <865514198@qq.com>
2022-01-27 14:56:37 +0800

    迁库

--- Website/DotNetCore3.1/SchoolManagement.EntityFrameworkCore/Migrations/20220127065537_add_todoitem.Designer.cs
+++ Website/DotNetCore3.1/SchoolManagement.EntityFrameworkCore/Migrations/20220127065537_add_todoitem.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolManagement.EntityFrameworkCore;
 
 namespace SchoolManagement.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220127065537_add_todoitem")]
+    partial class add_todoitem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Website/DotNetCore3.1/SchoolManagement.EntityFrameworkCore/Migrations/20220127065537_add_todoitem.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolManagement.EntityFrameworkCore.Migrations
+{
+    public partial class add_todoitem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                schema: "SchoolManagement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsComplete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItem",
+                schema: "SchoolManagement");
+        }
+    }
+}
