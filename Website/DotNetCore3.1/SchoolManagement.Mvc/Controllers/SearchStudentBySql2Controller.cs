ca239b4042a124c2f5c71fdc777561e3acae6d0d
lulu <865514198@qq.com>
2022-01-27 14:53:08 +0800

    联表查询

--- Website/DotNetCore3.1/SchoolManagement.Mvc/Controllers/SearchStudentBySql2Controller.cs
+++ Website/DotNetCore3.1/SchoolManagement.Mvc/Controllers/SearchStudentBySql2Controller.cs
@@ -1,8 +1,8 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using SchoolManagement.Core;
+﻿using Microsoft.AspNetCore.Mvc;
 using SchoolManagement.Core.Models;
 using SchoolManagement.Core.Repositories;
+using System.Threading.Tasks;
+using SchoolManagement.Core;
 
 namespace SchoolManagement.Mvc.Controllers
 {
@@ -17,8 +17,21 @@ namespace SchoolManagement.Mvc.Controllers
             _studentRepository = studentRepository;
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> SearchStudentBySql(int id)
+        [HttpGet("GetScore/{courseId}/{studentId}")]
+        public async Task<IActionResult> GetScoreBySql(int courseId, int studentId)
+        {
+            string sql = $"select sc.{nameof(Scroe.ScroeNumber)}" + "\n" +
+                $"from {SchoolManagementConsts.SchemaName}.{nameof(Course)} c" + "\n" +
+                $"join {SchoolManagementConsts.SchemaName}.{nameof(Scroe)} sc on c.{nameof(Course.Id)} = sc.{nameof(Scroe.CourseId)}" + "\n" +
+                $"join {SchoolManagementConsts.SchemaName}.{nameof(Student)} s on s.{nameof(Scroe.Id)} = sc.{nameof(Scroe.StudentId)}" + "\n" +
+                $"where c.{nameof(Course.Id)} = {courseId}" + "\n" +
+                $"and s.{nameof(Student.Id)} = {studentId}";
+            var data = await _studentRepository.ExecuteReaderAsync(sql);
+            return new JsonResult(data);
+        }
+
+         [HttpGet("GetStudentById/{id}")]
+        public async Task<IActionResult> GetStudentById(int id)
         {
             string sql = $"SELECT * FROM {SchoolManagementConsts.SchemaName}.{nameof(Student)} WHERE  {nameof(Student.Id)} ={id}";
             var data = await _studentRepository.ExecuteReaderAsync(sql);
