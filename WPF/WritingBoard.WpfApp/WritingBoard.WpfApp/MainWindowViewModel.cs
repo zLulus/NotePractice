37b0e8a496bbf5c79500a7cdb07d0f00b41e7b4f
15975588886@163.com <15975588886@163.com>
2022-02-06 17:39:20 +0800

    WritingBoard.WpfApp

--- WPF/WritingBoard.WpfApp/WritingBoard.WpfApp/MainWindowViewModel.cs
+++ WPF/WritingBoard.WpfApp/WritingBoard.WpfApp/MainWindowViewModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Ink;
 using System.Windows.Input;
 using WritingBoard.WpfApp.Commands;
