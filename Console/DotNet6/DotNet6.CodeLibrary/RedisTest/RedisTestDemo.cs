b84a975efb892596da85ab5d0b9eb218a2322dd9
lulu <865514198@qq.com>
2022-02-24 15:59:15 +0800

    RedisTestDemo

---
+++ Console/DotNet6/DotNet6.CodeLibrary/RedisTest/RedisTestDemo.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet6.CodeLibrary.RedisTest
+{
+    public class RedisTestDemo
+    {
+        public static async Task Run()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        }
+    }
+}
