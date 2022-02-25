bea5f8cc891ebf10b345064b1773d27c4a41f2c7
lulu <865514198@qq.com>
2022-02-25 16:46:35 +0800

    ListTest

---
+++ Console/DotNet6/DotNet6.CodeLibrary/ConcurrentTest/ConcurrentTestDemo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet6.CodeLibrary.ConcurrentTest
+{
+    public static class ConcurrentTestDemo
+    {
+        public static async Task Run()
+        {
+            await ListTest();
+        }
+
+        private static async Task ListTest()
+        {
+            List<int> list=new List<int>();
+            //ok
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    await Task.Run(() =>
+            //    {
+            //        list.Add(i);
+            //    });
+            //}
+
+            //Collection was modified; enumeration operation may not execute.
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    var t= Task.Run(() => { list.Add(i); });
+            //}
+
+            //Collection was modified; enumeration operation may not execute.
+            for (int i = 0; i < 10; i++)
+            {
+                new Thread(() => { list.Add(i); }).Start();
+            }
+
+
+
+            foreach (var data in list)
+                Console.WriteLine(data);
+        }
+    }
+}
--- Console/DotNet6/DotNet6.Console/Program.cs
+++ Console/DotNet6/DotNet6.Console/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DotNet6.CodeLibrary.ConcurrentTest;
 using DotNet6.CodeLibrary.RedisTest;
 
-await RedisTestDemo.Run();
+//await RedisTestDemo.Run();
+
+await ConcurrentTestDemo.Run();
 
 Console.ReadLine();
