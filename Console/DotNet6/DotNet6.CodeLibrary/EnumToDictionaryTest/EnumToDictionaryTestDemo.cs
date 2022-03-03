670dc73ad06f1c9b3e6768011354d161a2cb4577
lulu <865514198@qq.com>
2022-03-03 10:21:04 +0800

    EnumToDictionary

---
+++ Console/DotNet6/DotNet6.CodeLibrary/EnumToDictionaryTest/EnumToDictionaryTestDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet6.CodeLibrary.EnumToDictionaryTest
+{
+    public enum EnumToDictionaryEnum
+    {
+        A=1,
+        B=2,
+        C=3,
+        D=4,
+        E=5
+    }
+
+    public class EnumToDictionaryTestDemo
+    {
+        public static void Run()
+        {
+            FieldInfo[] fields = typeof(EnumToDictionaryEnum)
+               .GetFields(BindingFlags.Static | BindingFlags.Public)
+               ?? Array.Empty<FieldInfo>();
+
+            var dictionary = fields.ToDictionary(k => k.Name, v => (int)v.GetValue(null));
+
+            var dictionary2 = Enum.GetValues(typeof(EnumToDictionaryEnum))
+               .Cast<EnumToDictionaryEnum>()
+               .ToDictionary(k => k.ToString(), v => (int)v);
+        }
+    }
+}
--- Console/DotNet6/DotNet6.Console/Program.cs
+++ Console/DotNet6/DotNet6.Console/Program.cs
@@ -1,29 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DotNet6.CodeLibrary.ConcurrentTest;
 using DotNet6.CodeLibrary.DesignPatterns.BuilderPatternTest;
+using DotNet6.CodeLibrary.EnumToDictionaryTest;
 using DotNet6.CodeLibrary.FlagsTest;
 using DotNet6.CodeLibrary.ReaderWriterLockTest;
 using DotNet6.CodeLibrary.RedisTest;
 using DotNet6.CodeLibrary.ReferenceTest;
 using DotNet6.CodeLibrary.ReflectionPerformanceTest;
 
+//redis
 //await RedisTestDemo.Run();
 
+//线程安全
 //await ConcurrentTestDemo.Run();
 
+//锁
+//ReaderWriterLock & ReaderWriterLockSlim
 //ReaderWriterLockTestDemo.Run();
 //ReaderWriterLockSlimTestDemo.Run();
+//lock & Monitor
 //LockTestDemo.Run();
 //MonitorTestDemo.Run();
+//乐观锁
+//OptimisticLockTestDemo.Run();
 
+//flags特性
 //FlagsTestDemo.Run();
 
+//反射性能优化
 //ReflectionPerformanceTestDemo.Run();
 
+//设计模式
+//建造者模式
 //BuilderPatternTestDemo.Run();
 
+//引用类型和值类型
 //ReferenceTestDemo.Run();
 
-OptimisticLockTestDemo.Run();
+//枚举转字典
+EnumToDictionaryTestDemo.Run();
+
 
 Console.ReadLine();
