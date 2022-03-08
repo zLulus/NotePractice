bb1028fe550a74d1124c7e2ba9a35947be008502
lulu <865514198@qq.com>
2022-03-09 07:51:46 +0800

    分类

--- ML .NET/CatAndDogClassification/CatAndDogClassification.Client/MainWindow.xaml.cs
+++ ML .NET/CatAndDogClassification/CatAndDogClassification.Client/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using CatAndDogClassification_Train;
+using Microsoft.Win32;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CatAndDogClassification.Client
 {
@@ -20,9 +10,41 @@ namespace CatAndDogClassification.Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainWindowViewModel vm { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+
+            vm = new MainWindowViewModel();
+            DataContext = vm;
+        }
+
+        private void CatAndDogClassification_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                var output = CatAndDogMLModel.Predict(new CatAndDogMLModel.ModelInput()
+                {
+                    ImageSource = vm.SrcImagePath
+                });
+                Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show($"分类结果为:{output.Prediction}");
+                });
+                vm.ClassificationResult = output.Prediction;
+                vm.Score = output.Score;
+            });
+        }
+
+        private void SelectSrcImagePath_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "jpg文件(*.jpg)|*.jpg";
+            var isOk = file.ShowDialog();
+            if (isOk.HasValue && isOk.Value)
+            {
+                vm.SrcImagePath = file.FileName;
+            }
         }
     }
 }
---
+++ ML .NET/CatAndDogClassification/CatAndDogClassification.Client/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatAndDogClassification.Client
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private string srcImagePath;
+        public string SrcImagePath
+        {
+            set
+            {
+                srcImagePath = value;
+                OnPropertyChanged(nameof(SrcImagePath));
+            }
+            get
+            {
+                return srcImagePath;
+
+            }
+        }
+
+        private string classificationResult;
+        public string ClassificationResult
+        {
+            set
+            {
+                classificationResult = value;
+                OnPropertyChanged(nameof(ClassificationResult));
+            }
+            get
+            {
+                return classificationResult;
+
+            }
+        }
+
+        public float[] Score { get; set; }
+    }
+}
