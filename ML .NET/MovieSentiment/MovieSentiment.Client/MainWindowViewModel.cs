e0444bf55ed7fe7d800923388b310c3ac9a74843
lulu <865514198@qq.com>
2022-03-18 14:45:50 +0800

    ML .NET 情绪分析

---
+++ ML .NET/MovieSentiment/MovieSentiment.Client/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSentiment.Client
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private string review;
+        /// <summary>
+        /// 评论
+        /// </summary>
+        public string Review
+        {
+            set
+            {
+                review = value;
+                OnPropertyChanged(nameof(Review));
+            }
+            get
+            {
+                return review;
+            }
+        }
+
+        private string predictionSentiment;
+        /// <summary>
+        /// 预测结果
+        /// </summary>
+        public string PredictionSentiment
+        {
+            set
+            {
+                predictionSentiment = value;
+                OnPropertyChanged(nameof(PredictionSentiment));
+            }
+            get
+            {
+                return predictionSentiment;
+            }
+        }
+    }
+}
