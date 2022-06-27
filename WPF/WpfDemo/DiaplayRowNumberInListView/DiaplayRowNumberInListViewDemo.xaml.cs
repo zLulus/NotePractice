16a856594a11da6245f12234916a1cba6946a5f5
luna <luna.zeng@audatex.com>
2022-06-27 21:26:12 +0800

    DiaplayRowNumberInListViewDemo

---
+++ WPF/WpfDemo/DiaplayRowNumberInListView/DiaplayRowNumberInListViewDemo.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfDemo.DiaplayRowNumberInListView
+{
+    /// <summary>
+    /// Interaction logic for DiaplayRowNumberInListViewDemo.xaml
+    /// </summary>
+    public partial class DiaplayRowNumberInListViewDemo : UserControl
+    {
+        public DiaplayRowNumberInListViewDemo()
+        {
+            InitializeComponent();
+
+            var datas = new ObservableCollection<DataModel>();
+            datas.Add(new DataModel() { Name = "Jenny" });
+            datas.Add(new DataModel() { Name = "Bill" });
+            datas.Add(new DataModel() { Name = "Blue" });
+            datas.Add(new DataModel() { Name = "blablaName" });
+            listviewNames.DataContext = datas;
+        }
+
+        private void MoveUp_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null)
+                return;
+            DataModel data = button.CommandParameter as DataModel;
+            if (data == null)
+                return;
+            ObservableCollection<DataModel> behaviors = listviewNames.DataContext as ObservableCollection<DataModel>;
+            if (behaviors == null)
+                return;
+            int index = index = behaviors.IndexOf(data);
+            var newIndex = index > 0 ? index - 1 : index;
+            behaviors.Move(index, newIndex);
+        }
+
+        private void MoveDown_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null)
+                return;
+            DataModel data = button.CommandParameter as DataModel;
+            if (data == null)
+                return;
+            ObservableCollection<DataModel> behaviors = listviewNames.DataContext as ObservableCollection<DataModel>;
+            if (behaviors == null)
+                return;
+            int index = = behaviors.IndexOf(data);
+            var newIndex = index < behaviors.Count - 1 ? index + 1 : index;
+            behaviors.Move(index, newIndex);
+        }
+
+        private class DataModel
+        {
+            public string Name { get; set; }
+        }
+    }
+
+
+}
