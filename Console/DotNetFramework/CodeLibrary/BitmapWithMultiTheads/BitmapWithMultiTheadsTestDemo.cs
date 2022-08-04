4ff3a0db189bc10aef977bb187dc3acebf524b58
luna <luna.zeng@audatex.com>
2022-08-04 21:01:07 +0800

    BitmapWithMultiTheadsTestDemo

--- Console/DotNetFramework/CodeLibrary/BitmapWithMultiTheads/BitmapWithMultiTheadsTestDemo.cs
+++ Console/DotNetFramework/CodeLibrary/BitmapWithMultiTheads/BitmapWithMultiTheadsTestDemo.cs
@@ -15,7 +15,46 @@ namespace CodeLibrary.BitmapWithMultiTheads
         public static void Run()
         {
             //Test1();
-            Test2();
+            //Test2();
+            Test3();
+        }
+
+        private static void Test3()
+        {
+            var bmp = new Bitmap($"{Directory.GetCurrentDirectory()}\\BitmapWithMultiTheads\\OIP-C.png");
+            var t = new Task(() =>
+            {
+                Method3(bmp);
+            });
+            var t2 = new Task(() =>
+            {
+                Method3(bmp);
+            });
+            t.Start();
+            t2.Start();
+        }
+
+        private static void Method3(Bitmap bmp)
+        {
+            Bitmap b1 = null;
+            try
+            {
+                //lock (lockObject)
+                    b1 = bmp.Clone(new Rectangle(0, 0, bmp.Width, bmp.Height), bmp.PixelFormat);
+                //using (MemoryStream ms = new MemoryStream())
+                //{
+                //    b1.Save(ms, ImageFormat.Png);
+                //    var bytes1 = ms.GetBuffer();
+                //}
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                b1?.Dispose();
+            }
         }
 
         private static void Test2()
@@ -23,38 +62,31 @@ namespace CodeLibrary.BitmapWithMultiTheads
             var bmp = new Bitmap($"{Directory.GetCurrentDirectory()}\\BitmapWithMultiTheads\\OIP-C.png");
             var t = new Task(() =>
             {
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    object b1 = null;
-                    lock (lockObject)
-                        b1 = bmp.Clone();
-                    if (b1 != null)
-                    {
-                        var s = (b1 as Bitmap);
-                        s.Save(ms, ImageFormat.Png);
-                        var bytes1 = ms.GetBuffer();
-                        s?.Dispose();
-                    }
-                }
+                Method2(bmp);
             });
             var t2 = new Task(() =>
             {
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    object b1 = null;
-                    lock (lockObject)
-                        b1 = bmp.Clone();
-                    if (b1 != null)
-                    {
-                        var s = (b1 as Bitmap);
-                        s.Save(ms, ImageFormat.Png);
-                        var bytes1 = ms.GetBuffer();
-                        s?.Dispose();
-                    }
-                }
+                Method2(bmp);
             });
             t.Start();
             t2.Start();
+        }
+
+        private static void Method2(Bitmap bmp)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                object b1 = null;
+                lock (lockObject)
+                    b1 = bmp.Clone();
+                if (b1 != null)
+                {
+                    var s = (b1 as Bitmap);
+                    s.Save(ms, ImageFormat.Png);
+                    var bytes1 = ms.GetBuffer();
+                    s?.Dispose();
+                }
+            }
         }
 
         private static void Test1()
@@ -62,28 +94,26 @@ namespace CodeLibrary.BitmapWithMultiTheads
             var bmp = new Bitmap($"{Directory.GetCurrentDirectory()}\\BitmapWithMultiTheads\\OIP-C.png");
             var t = new Task(() =>
             {
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    lock (lockObject)
-                    {
-                        bmp.Save(ms, ImageFormat.Png);
-                    }
-                    var bytes1 = ms.GetBuffer();
-                }
+                Method1(bmp);
             });
             var t2 = new Task(() =>
             {
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    lock (lockObject)
-                    {
-                        bmp.Save(ms, ImageFormat.Png);
-                    }
-                    var bytes2 = ms.GetBuffer();
-                }
+                Method1(bmp);
             });
             t.Start();
             t2.Start();
+        }
+
+        private static void Method1(Bitmap bmp)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                lock (lockObject)
+                {
+                    bmp.Save(ms, ImageFormat.Png);
+                }
+                var bytes2 = ms.GetBuffer();
+            }
         }
     }
 }
