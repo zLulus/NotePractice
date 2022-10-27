eedce3859841eb3c13a1ab0831c5f0da7e2d33a6
luna <luna.zeng@audatex.com>
2022-10-27 13:43:53 +0800

    code reference

--- WPF/WpfDemo/SearchReferences/Models/CodeReferenceInfo.cs
+++ WPF/WpfDemo/SearchReferences/Models/CodeReferenceInfo.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace WpfDemo.SearchReferences.Models
 {
     internal class CodeReferenceInfo
     {
         internal string FilePath { get; set; }
+        internal string FileName { get; set; }
         internal string MethodName { get; set; }
         internal int MethodParameterCount { get; set; }
         internal string Code { get; set; }
         /// <summary>
-        /// FilePath_MethodName_MethodParameterCount
+        /// FilePath,InstanceName,MethodName_MethodParameterCount
         /// </summary>
-        internal List<string> References { get; set; }
+        internal List<Tuple<string, string, string>> References { get; set; }
+        internal List<string> ReferencesDictionary { get; set; }
     }
 }
--- WPF/WpfDemo/SearchReferences/SearchReferencesTool.xaml.cs
+++ WPF/WpfDemo/SearchReferences/SearchReferencesTool.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,9 +33,22 @@ namespace WpfDemo.SearchReferences
             Dictionary<string, CodeReferenceInfo> codeReferenceInfos = new Dictionary<string, CodeReferenceInfo>();
             foreach (var file in files)
             {
+                Regex fileRegex = new Regex(@"\\(?<fileName>.+?).cs", RegexOptions.Compiled);
+                var fileMatch = fileRegex.Match(file);
+                if (!fileMatch.Success)
+                {
+                    continue;
+                }
+                var fileName = fileMatch.Groups["fileName"].Value;
+                if (fileName.Contains("\\"))
+                {
+                    var s = fileName.Split('\\');
+                    fileName = s[s.Length - 1];
+                }
+
                 var content = File.ReadAllText(file);
-                //todo
-                Regex regex = new Regex(@"internal(?<methodName>.+?)[\s\S]+?\((?<parameters>.+?)\)[\s\S]+?\{", RegexOptions.Compiled);
+                //todo 没有检查没有private关键词的method，没有检查methodName和(parameters)之间有空格的method
+                Regex regex = new Regex(@"(internal|public|private)\s+?(?<methodName>.+?)\((?<parameters>.+?)\)[\s\S]+?\{", RegexOptions.Compiled);
                 var matches = regex.Matches(content);
                 if (matches.Count > 0)
                 {
@@ -42,18 +56,30 @@ namespace WpfDemo.SearchReferences
                     foreach (Match match in matches)
                     {
                         string methodName = match.Groups["methodName"].Value;
+                        if (methodName.Contains(" "))
+                        {
+                            var mmm = methodName.Split(' ');
+                            methodName = mmm[mmm.Length - 1];
+                        }
                         string parameters = match.Groups["parameters"].Value;
+                        int parameterCount = GetParameterCount(parameters);
                         t.Add(new Tuple<string, string>(methodName, parameters));
-                        //todo
-                        //var info = GetParameterCountAndReferencesAndCode(methodName, content);
-                        //codeReferenceInfos.Add($"{file}_{methodName}_{info.Item1}", new CodeReferenceInfo()
-                        //{
-                        //    FilePath = file,
-                        //    MethodName = methodName,
-                        //    MethodParameterCount = info.Item1,
-                        //    Code = info.Item3,
-                        //    References = info.Item2
-                        //});
+                        var info = GetReferencesAndCode(methodName, parameters, content);
+
+                        //todo 同名同参数个数
+                        var key = $"{file}_{methodName}_{parameterCount}";
+                        if (!codeReferenceInfos.ContainsKey(key))
+                        {
+                            codeReferenceInfos.Add(key, new CodeReferenceInfo()
+                            {
+                                FilePath = file,
+                                FileName = fileName,
+                                MethodName = methodName,
+                                MethodParameterCount = parameterCount,
+                                Code = info.Item2,
+                                References = info.Item1
+                            });
+                        }
                     }
                 }
 
@@ -67,14 +93,15 @@ namespace WpfDemo.SearchReferences
                 {
                     foreach (var key in codeReferenceInfos.Keys)
                     {
-                        if (key.Contains($"{reference}"))
+                        if (key.Contains($"{reference.Item3}") && reference.Item2.Contains(codeReferenceInfos[key].FileName.ToLower()))
                         {
-                            newReferences.Add(key);
+                            if (!newReferences.Contains(key))
+                                newReferences.Add(key);
                         }
                     }
                 }
                 //_MethodName_MethodParameterCount -> FilePath_MethodName_MethodParameterCount
-                codeReferenceInfo.Value.References = newReferences;
+                codeReferenceInfo.Value.ReferencesDictionary = newReferences;
             }
 
             //根据关键词查询相关数据
@@ -85,28 +112,109 @@ namespace WpfDemo.SearchReferences
             {
                 if (codeReferenceInfo.Value.Code.Contains($" {searchWord} "))
                 {
-                    List<CodeReferenceInfo> referenceList = new List<CodeReferenceInfo>();
+                    //一个引用链
+                    List<string> referenceList = new List<string>();
+                    referenceList.Add(codeReferenceInfo.Key);
                     GetReferenceList(referenceList, codeReferenceInfos, codeReferenceInfo);
                 }
             }
         }
 
-        private void GetReferenceList(List<CodeReferenceInfo> referenceList, Dictionary<string, CodeReferenceInfo> codeReferenceInfos, KeyValuePair<string, CodeReferenceInfo> target)
+        private static int GetParameterCount(string parameters)
+        {
+            var parameterCount = 0;
+            if (string.IsNullOrEmpty(parameters) || string.IsNullOrWhiteSpace(parameters))
+            {
+                parameterCount = 0;
+            }
+            else
+            {
+                if (parameters.Contains(","))
+                {
+                    parameterCount = parameters.Split(',').Length;
+                }
+                else
+                {
+                    parameterCount = 1;
+                }
+            }
+
+            return parameterCount;
+        }
+
+        private void GetReferenceList(List<string> referenceList, Dictionary<string, CodeReferenceInfo> codeReferenceInfos, KeyValuePair<string, CodeReferenceInfo> target)
         {
             foreach (var codeReferenceInfo in codeReferenceInfos)
             {
-                if (codeReferenceInfo.Value.References.Contains(target.Key))
+                if (codeReferenceInfo.Value.ReferencesDictionary.Contains(target.Key))
                 {
-                    referenceList.Add(codeReferenceInfo.Value);
+                    referenceList.Add(codeReferenceInfo.Key);
                     GetReferenceList(referenceList, codeReferenceInfos, codeReferenceInfo);
                 }
             }
         }
 
-        private Tuple<int, List<string>, string> GetParameterCountAndReferencesAndCode(string methodName, string content)
+        private Tuple<List<Tuple<string, string, string>>, string> GetReferencesAndCode(string methodName, string parameters, string content)
         {
             //返回reference为:_MethodName_MethodParameterCount
-            return null;
+            Stack<string> stack = new Stack<string>();
+            bool isFirst = true;
+            //todo 没有检查methodName和(parameters)之间有空格的method
+            string start = $"{methodName}({parameters})";
+            var startIndex = content.IndexOf(start);
+            var endIndex = startIndex;
+            StringBuilder sb = new StringBuilder();
+            while (stack.Count != 0 || isFirst)
+            {
+                var s = content[endIndex];
+                if (s == '{')
+                {
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                    }
+                    stack.Push("{");
+                }
+                else if (s == '}')
+                {
+                    if (stack.Peek() == "{")
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push("}");
+                    }
+                }
+                if (!isFirst)
+                    sb.Append(s);
+                endIndex++;
+            }
+            var code = sb.ToString();
+
+            List<Tuple<string, string, string>> referenceList = new List<Tuple<string, string, string>>();
+            //todo 没有检查没有private关键词的method，没有检查methodName和(parameters)之间有空格的method
+            Regex regex = new Regex(@"\s(?<referenceInstanceName>.+?)\.(?<referenceMethodName>.+?)\((?<referenceParameters>.+?)\)", RegexOptions.Compiled);
+            var matches = regex.Matches(code);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    string referenceInstanceName = match.Groups["referenceInstanceName"].Value;
+                    if (referenceInstanceName.Contains(" "))
+                    {
+                        var s = referenceInstanceName.Split(' ');
+                        referenceInstanceName = s[s.Length - 1];
+                    }
+                    string referenceMethodName = match.Groups["referenceMethodName"].Value;
+                    string referenceParameters = match.Groups["referenceParameters"].Value;
+                    int parameterCount = GetParameterCount(referenceParameters);
+                    var newReference = $"_{referenceMethodName}_{parameterCount}";
+                    referenceList.Add(new Tuple<string, string, string>("", referenceInstanceName.ToLower(), newReference));
+                }
+            }
+
+            return new Tuple<List<Tuple<string, string, string>>, string>(referenceList, code);
         }
 
         private static List<string> GetAllCsFiles(string folderPath)
