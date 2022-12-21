a7563d67d31ca544059234c541a1da84d31f81d1
luna <luna.zeng@audatex.com>
2022-12-21 21:18:23 +0800

    feat: add IsLeapYear method and unit test

---
+++ UnitTest/UnitTest.ClassLibrary/YearService.cs
@@ -0,0 +1,18 @@
+﻿namespace UnitTest.ClassLibrary
+{
+    public class YearService
+    {
+        public bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0)
+                if (year % 100 == 0)
+                    if (year % 400 == 0)
+                        return true;
+                    else
+                        return false;
+                else
+                    return true;
+            return false;
+        }
+    }
+}
---
+++ UnitTest/XUnit.Coverlet.Collector/YearServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using UnitTest.ClassLibrary;
+
+namespace XUnit.Coverlet.Collector
+{
+    public class YearServiceTest
+    {
+        readonly YearService _yearService;
+
+        public YearServiceTest() => _yearService = new YearService();
+
+        [
+            Theory,
+            InlineData(2000), InlineData(1600)
+        ]
+        public void IsLeapYear_WholeCenturyOfNumbers_ReturnTrue(int value) =>
+            Assert.True(_yearService.IsLeapYear(value), $"{value} is leap year.");
+
+        [
+            Theory,
+            InlineData(1900), InlineData(1700)
+        ]
+        public void IsLeapYear_WholeCenturyOfNumbers_ReturnFalse(int value) =>
+            Assert.False(_yearService.IsLeapYear(value), $"{value} is not leap year.");
+
+        [
+           Theory,
+           InlineData(2020)
+       ]
+        public void IsPrime_NotWholeCenturyOfNumbers_ReturnTrue(int value) =>
+           Assert.True(_yearService.IsLeapYear(value), $"{value} is leap year.");
+
+        [
+            Theory,
+            InlineData(2022), InlineData(2003)
+        ]
+        public void IsPrime_NotWholeCenturyOfNumbers_ReturnFalse(int value) =>
+            Assert.False(_yearService.IsLeapYear(value), $"{value} is not leap year.");
+    }
+}
