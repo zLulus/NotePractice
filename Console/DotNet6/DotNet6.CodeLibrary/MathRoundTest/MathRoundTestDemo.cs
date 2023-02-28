f9bad7852a8988b75e0b56a82c6e2932a8c17777
luna <luna.zeng@audatex.com>
2023-02-28 22:35:48 +0800

    feat: Math.Round

--- Console/DotNet6/DotNet6.CodeLibrary/MathRoundTest/MathRoundTestDemo.cs
+++ Console/DotNet6/DotNet6.CodeLibrary/MathRoundTest/MathRoundTestDemo.cs
@@ -20,6 +20,14 @@
             var toNegativeInfinityNumber2 = Math.Round(originalNumber2, 2, MidpointRounding.ToNegativeInfinity);//output:9.08
             var toPositiveInfinityNumber2 = Math.Round(originalNumber2, 2, MidpointRounding.ToPositiveInfinity);//output:9.09
 
+            var originalNumber3 = (decimal)9.085;
+            var nomalNumber3 = Math.Round(originalNumber3, 2);//output:9.08
+            var toEvenNumber3 = Math.Round(originalNumber3, 2, MidpointRounding.ToEven);//output:9.08
+            var awayFromZeroNumber3 = Math.Round(originalNumber3, 2, MidpointRounding.AwayFromZero);//output:9.09
+            var toZeroNumber3 = Math.Round(originalNumber3, 2, MidpointRounding.ToZero);//output:9.08
+            var toNegativeInfinityNumber3 = Math.Round(originalNumber3, 2, MidpointRounding.ToNegativeInfinity);//output:9.08
+            var toPositiveInfinityNumber3 = Math.Round(originalNumber3, 2, MidpointRounding.ToPositiveInfinity);//output:9.09
+
 
         }
     }
