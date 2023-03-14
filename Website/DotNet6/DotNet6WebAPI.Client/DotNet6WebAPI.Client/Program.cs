ea31689eb0255b8484ec1f9b4591590f905e7402
luna <luna.zeng@audatex.com>
2023-03-14 19:03:36 +0800

    feat: add client project

---
+++ Website/DotNet6/DotNet6WebAPI.Client/DotNet6WebAPI.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+using TestClientNamespace;
+
+HttpClient httpClient = new HttpClient();
+TestClient client = new TestClient("http://localhost:5137/", httpClient);
+var enums = await client.GetEnumAsync();
+Console.WriteLine(JsonConvert.SerializeObject(enums));
+
+Console.ReadLine();
