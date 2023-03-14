6a66a06938adb152ac1ca6eb63333a6f20febb66
luna <luna.zeng@audatex.com>
2023-03-14 19:23:56 +0800

    feat: friend assemblies demo

---
+++ UnitTest/UnitTest.ClassLibrary/InternalTestService.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("XUnit.Coverlet.Collector")]
+namespace UnitTest.ClassLibrary
+{
+    internal class InternalTestService
+    {
+        internal int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ UnitTest/XUnit.Coverlet.Collector/InternalTestServiceUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using Shouldly;
+using UnitTest.ClassLibrary;
+
+namespace XUnit.Coverlet.Collector
+{
+    public class InternalTestServiceUnitTest
+    {
+        [Fact]
+        public void Add()
+        {
+            InternalTestService internalTestService = new InternalTestService();
+            internalTestService.Add(1, 2).ShouldBe(3);
+        }
+    }
+}
