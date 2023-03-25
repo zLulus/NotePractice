0d33572a978cd77cd36d317c9aa22e21c918ad2f
Luna <luna.zeng@audatex.com>
2023-03-25 15:24:48 +0800

    feat: layouts

--- MAUI/Maui.Demo/Maui.Demo/Pages/Layouts/BindableLayoutPage.xaml.cs
+++ MAUI/Maui.Demo/Maui.Demo/Pages/Layouts/BindableLayoutPage.xaml.cs
@@ -1,3 +1,6 @@
+using Maui.Demo.Pages.Layouts.ViewModels;
+using System.Collections.ObjectModel;
+
 namespace Maui.Demo.Pages.Layouts;
 
 public partial class BindableLayoutPage : ContentPage
@@ -5,5 +8,18 @@ public partial class BindableLayoutPage : ContentPage
 	public BindableLayoutPage()
 	{
 		InitializeComponent();
+
+		ObservableCollection<User> users = new ObservableCollection<User>();
+		users.Add(new User()
+		{
+			Name = "user 1",
+			ImagePath = "cat.png"
+		});
+		users.Add(new User()
+		{
+			Name = "user 2",
+			ImagePath = "cat.png"
+		});
+		userList.BindingContext = users;
 	}
 }
---
+++ MAUI/Maui.Demo/Maui.Demo/Pages/Layouts/ViewModels/User.cs
@@ -0,0 +1,35 @@
+﻿using Maui.Demo.Pages.Bindings.ViewModels;
+
+namespace Maui.Demo.Pages.Layouts.ViewModels
+{
+    public class User : BaseNotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                name = value;
+                Notification(nameof(Name));
+            }
+        }
+
+        private string imagePath;
+        public string ImagePath
+        {
+            get
+            {
+                return imagePath;
+            }
+            private set
+            {
+                imagePath = value;
+                Notification(nameof(ImagePath));
+            }
+        }
+    }
+}
