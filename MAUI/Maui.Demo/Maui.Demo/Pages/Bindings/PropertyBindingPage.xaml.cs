6fe55769e5c18677afca9c37a11defeb47b3e9cf
Luna <luna.zeng@audatex.com>
2023-03-25 17:11:16 +0800

    feat: PropertyBindingPage

--- MAUI/Maui.Demo/Maui.Demo/Pages/Bindings/PropertyBindingPage.xaml.cs
+++ MAUI/Maui.Demo/Maui.Demo/Pages/Bindings/PropertyBindingPage.xaml.cs
@@ -1,11 +1,8 @@
 namespace Maui.Demo.Pages.Bindings;
 
-/// <summary>
-/// https://learn.microsoft.com/zh-cn/dotnet/maui/fundamentals/data-binding/basic-bindings?view=net-maui-7.0&WT.mc_id=DT-MVP-5003010
-/// </summary>
-public partial class BindingRotationAndScalePage : ContentPage
+public partial class PropertyBindingPage : ContentPage
 {
-    public BindingRotationAndScalePage()
+    public PropertyBindingPage()
     {
         InitializeComponent();
 
