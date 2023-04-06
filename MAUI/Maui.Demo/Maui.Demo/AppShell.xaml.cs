a300c8e001a26d781c92768bf715b6b1b878fd66
Luna <luna.zeng@audatex.com>
2023-04-06 20:03:38 +0800

    feat: adjust AppShell

--- MAUI/Maui.Demo/Maui.Demo/AppShell.xaml.cs
+++ MAUI/Maui.Demo/Maui.Demo/AppShell.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Maui.Demo;
+﻿using Maui.Demo.Pages.Triggers;
+
+namespace Maui.Demo;
 
 /// <summary>
 /// https://learn.microsoft.com/zh-cn/dotnet/maui/fundamentals/shell/?view=net-maui-7.0&WT.mc_id=DT-MVP-5003010
@@ -8,5 +10,8 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
+
+        Routing.RegisterRoute(nameof(MultiTriggersPage), typeof(MultiTriggersPage));
+        Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
     }
 }
