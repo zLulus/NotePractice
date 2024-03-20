47aa6d828a4c9703f9872801ecdc01fb314860b2
Luna <865514198@qq.com>
2024-03-20 20:46:26 +0800

    feat: Blazor demo

---
+++ Blazor/BlazorDemoApp/BlazorDemoApp/Program.cs
@@ -0,0 +1,26 @@
+using BlazorDemoApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+//builder.Services.AddRazorPages();
+//builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
