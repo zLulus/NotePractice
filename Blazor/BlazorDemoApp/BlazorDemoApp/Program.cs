d1a21f88b277c024fb9406ae89a5d82bbbfc02db
Luna <865514198@qq.com>
2024-03-22 15:44:49 +0800

    feat: Blazor HttpClientExample

--- Blazor/BlazorDemoApp/BlazorDemoApp/Program.cs
+++ Blazor/BlazorDemoApp/BlazorDemoApp/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddSingleton<IDataService, DataService>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
